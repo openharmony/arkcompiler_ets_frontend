b6ca4f4aa950856a781d93166e31173dbd76fec5
jiangkaiwen <jiangkaiwen2@huawei.com>
2022-11-25 14:32:54 +0800

    Modify collect_module_dependencies() function for fixing 262 frame
    Issue:I634E1
    
    Signed-off-by: jiangkaiwen <jiangkaiwen2@huawei.com>
    Change-Id: I1380e3d58e64e8173810af50b39de9243ff0db30

--- test262/utils.py
+++ test262/utils.py
@@ -165,6 +165,7 @@ def search_dependency(file, directory):
         for f in files:
             if f == file:
                 return os.path.join(root, f)
+    return "FILE_NOT_FOUND"
 
 
 def collect_module_dependencies(file, directory, traversedDependencies):
@@ -172,15 +173,18 @@ def collect_module_dependencies(file, directory, traversedDependencies):
     traversedDependencies.append(file)
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
-        module_import_list = re.findall(r'(export|import|from)(?:\s*)\(?(\'(\.\/.*)\'|"(\.\/.*)")\)?', content)
+        module_import_list = re.findall(r'(import|from)(?:\s*)\(?(\'(\.\/.*)\'|"(\.\/.*)")\)?', content)
 
         for result in list(set(module_import_list)):
-            specifier = result[2] if len(result[2]) != 0 else result[3]
-            if re.search(r'\S+_FIXTURE.js$', specifier):
-                dependency = search_dependency(specifier.lstrip('./'), directory)
+            specifier = (result[2] if len(result[2]) != 0 else result[3]).lstrip('./')
+            if os.path.basename(file) is not specifier:
+                dependency = search_dependency(specifier, directory)
+                if dependency == "FILE_NOT_FOUND":
+                    continue
+
                 if dependency not in traversedDependencies:
                     dependencies.extend(collect_module_dependencies(dependency, directory,
-                                                                   list(set(traversedDependencies))))
+                                                                    list(set(traversedDependencies))))
                 dependencies.append(dependency)
 
     return dependencies
