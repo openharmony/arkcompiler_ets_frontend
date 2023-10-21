50bb26a8bab58c3551d0b8339d63989e306e9789
shixiaowei4 <shixiaowei4@huawei.com>
2023-10-21 16:46:41 +0800

    Remove AOT TYPE mode and fix disasm bug
    Signed-off-by: shixiaowei4 <shixiaowei4@huawei.com>

--- test/scripts/timer.py
+++ test/scripts/timer.py
@@ -26,21 +26,21 @@ def job(cmd):
     subprocess.run(cmd, shell=False)
 
 
+def run():
+    job(os.path.join(".", "auto_xts_test", "run.bat"))
+    job(f'python {os.path.join(".", "sdk_test", "entry.py")}')
+    if not os.path.exists("sdk_url.txt"):
+        print('SDK download failed')
+        return
+
+    job(f'python {os.path.join(".", "performance_test", "performance_entry.py")}')
+    job(f'python {os.path.join(".", "send_email.py")}')
+
+
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    # do preparations
-    schedule.every().day.at("01:00").do(job, cmd=os.path.join(
-        ".", "auto_xts_test", "run.bat")).tag('daily_xts_task')
-    # do sdk_test
-    schedule.every().day.at("01:00").do(
-        job, cmd=f'python {os.path.join(".", "sdk_test", "entry.py")}').tag('daily_sdk_task')
-    # do perf_test
-    schedule.every().day.at("01:00").do(
-        job, cmd=f'python {os.path.join(".", "performance_test", "performance_entry.py")}')
-    # send mail
-    schedule.every().day.at("01:00").do(
-        job, cmd=f'python {os.path.join(".", "send_email.py")}').tag("send_email")
-    schedule.run_all()
+    schedule.every().day.at("02:10").do(run)
+    run()
     while True:
         schedule.run_pending()
         time.sleep(1)
