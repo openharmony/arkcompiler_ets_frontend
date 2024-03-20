b5d5cbaa304d4980b0cbc79365188cca3c57fe85
Torok Gergo <gergo.torok@huawei.com>
2024-03-20 16:51:56 +0100

    [ETS] Reworking union type assignement checking
    Description:
    Fixing union assignment problems related to function type.
    Refactoring union creation and relations.
    
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I9FXVQ
    
    Testing: Union relations now work with the related functionns and follows the potential changes in other type assignabilities
    
    Signed-off-by: Gergo Torok <gergo.torok@huawei.com>

--- ets2panda/checker/ets/helpers.cpp
+++ ets2panda/checker/ets/helpers.cpp
@@ -1500,6 +1500,7 @@ Type *ETSChecker::MaybeBoxExpression(ir::Expression *expr)
     if (promoted != expr->TsType()) {
         expr->AddBoxingUnboxingFlags(GetBoxingFlag(promoted));
     }
+    Relation()->SetNode(expr);
     return promoted;
 }
 
