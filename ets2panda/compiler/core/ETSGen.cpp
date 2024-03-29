c23075d0a1cba56c4cc1fc870edec42addeff0a4
ozerovnikita <ozerov.nikita@huawei-partners.com>
2024-03-29 14:19:34 +0300

    fix undefined[] and null[]
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/I98NEJ
    
    Testing: test ets2panda/test/compiler ets2panda/test/runtime
    
    Signed-off-by: ozerovnikita <ozerov.nikita@huawei-partners.com>
    
    Accept-Changeset: 12987:fe:2002:rt:0

--- ets2panda/compiler/core/ETSGen.cpp
+++ ets2panda/compiler/core/ETSGen.cpp
@@ -2706,6 +2706,11 @@ void ETSGen::LoadArrayLength(const ir::AstNode *node, VReg arrayReg)
 void ETSGen::LoadArrayElement(const ir::AstNode *node, VReg objectReg)
 {
     auto *elementType = GetVRegType(objectReg)->AsETSArrayType()->ElementType();
+    if (elementType->IsETSReferenceType()) {
+        Ra().Emit<LdarrObj>(node, objectReg);
+        SetAccumulatorType(elementType);
+        return;
+    }
     switch (checker::ETSChecker::ETSType(elementType)) {
         case checker::TypeFlag::ETS_BOOLEAN:
         case checker::TypeFlag::BYTE: {
@@ -2739,14 +2744,6 @@ void ETSGen::LoadArrayElement(const ir::AstNode *node, VReg objectReg)
             Ra().Emit<FldarrWide>(node, objectReg);
             break;
         }
-        case checker::TypeFlag::ETS_ARRAY:
-        case checker::TypeFlag::ETS_OBJECT:
-        case checker::TypeFlag::ETS_TYPE_PARAMETER:
-        case checker::TypeFlag::ETS_UNION:
-        case checker::TypeFlag::ETS_DYNAMIC_TYPE: {
-            Ra().Emit<LdarrObj>(node, objectReg);
-            break;
-        }
 
         default: {
             UNREACHABLE();
@@ -2758,6 +2755,11 @@ void ETSGen::LoadArrayElement(const ir::AstNode *node, VReg objectReg)
 
 void ETSGen::StoreArrayElement(const ir::AstNode *node, VReg objectReg, VReg index, const checker::Type *elementType)
 {
+    if (elementType->IsETSReferenceType()) {
+        Ra().Emit<StarrObj>(node, objectReg, index);
+        SetAccumulatorType(elementType);
+        return;
+    }
     switch (checker::ETSChecker::ETSType(elementType)) {
         case checker::TypeFlag::ETS_BOOLEAN:
         case checker::TypeFlag::BYTE: {
@@ -2786,14 +2788,6 @@ void ETSGen::StoreArrayElement(const ir::AstNode *node, VReg objectReg, VReg ind
         }
         case checker::TypeFlag::DOUBLE: {
             Ra().Emit<FstarrWide>(node, objectReg, index);
-            break;
-        }
-        case checker::TypeFlag::ETS_ARRAY:
-        case checker::TypeFlag::ETS_OBJECT:
-        case checker::TypeFlag::ETS_TYPE_PARAMETER:
-        case checker::TypeFlag::ETS_UNION:
-        case checker::TypeFlag::ETS_DYNAMIC_TYPE: {
-            Ra().Emit<StarrObj>(node, objectReg, index);
             break;
         }
 
