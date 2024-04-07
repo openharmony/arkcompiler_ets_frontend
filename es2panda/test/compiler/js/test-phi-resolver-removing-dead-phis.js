965679eeef24f9f62b2a2b6df2c320047d6d538c
li-tao116 <litao@kaihong.com>
2024-04-07 15:02:21 +0800

    Add copyright item
    Signed-off-by: litao <litao@kaihong.com>

--- es2panda/test/compiler/js/test-phi-resolver-removing-dead-phis.js
+++ es2panda/test/compiler/js/test-phi-resolver-removing-dead-phis.js
@@ -10,7 +10,6 @@
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
- limitations under the License.
  *
  Copyright (c) 2024 Huawei Device Co., Ltd.
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,7 +22,7 @@
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
- limitations under the License.
+ limitations under the License.limitations under the License.
  */
 
 function loop1(a) {
