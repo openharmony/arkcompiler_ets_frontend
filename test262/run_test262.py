5dd48b368832b4fe44ec8c1b7adc3bc4dd2207fe
jiangkaiwen <jiangkaiwen2@huawei.com>
2024-05-23 11:24:00 +0800

    Fix the bug of missing function arguments
    Issue:I9RJ3F
    
    Signed-off-by: jiangkaiwen <jiangkaiwen2@huawei.com>
    Change-Id: Ia6d11747409ccf88a7f424e360b497294d4839a0

--- test262/run_test262.py
+++ test262/run_test262.py
@@ -570,7 +570,7 @@ def get_host_args_of_product_name(args):
     return ark_tool, libs_dir, ark_aot_tool, merge_abc_binary
 
 
-def get_host_args_of_host_type(args, host_type, ark_tool, ark_aot_tool, libs_dir, ark_frontend,
+def get_host_args_of_host_type(args, host_args, ark_tool, ark_aot_tool, libs_dir, ark_frontend,
                                ark_frontend_binary, opt_level, es2abc_thread_count,
                                merge_abc_binary, merge_abc_mode, product_name):
     host_args = f"-B test262/run_sunspider.py "
@@ -592,7 +592,7 @@ def get_host_args_of_host_type(args, host_type, ark_tool, ark_aot_tool, libs_dir
     return host_args
 
 
-def get_host_args_of_ark_arch(args):
+def get_host_args_of_ark_arch(args, host_args):
     host_args += f"--ark-arch={args.ark_arch} "
     host_args += f"--ark-arch-root={args.ark_arch_root} "
 
@@ -647,12 +647,12 @@ def get_host_args(args, host_type):
         merge_abc_mode = args.merge_abc_mode
 
     if host_type == DEFAULT_HOST_TYPE:
-        host_args = get_host_args_of_host_type(args, host_type, ark_tool, ark_aot_tool, libs_dir, ark_frontend,
+        host_args = get_host_args_of_host_type(args, host_args, ark_tool, ark_aot_tool, libs_dir, ark_frontend,
                                                ark_frontend_binary, opt_level, es2abc_thread_count,
                                                merge_abc_binary, merge_abc_mode, product_name)
 
     if args.ark_arch != ark_arch:
-        host_args = get_host_args_of_ark_arch(args)
+        host_args = get_host_args_of_ark_arch(args, host_args)
 
     return host_args
 
