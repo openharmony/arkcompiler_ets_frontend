369df08b96eb15a0e0cb9611c7d5ceb1321e9afd
yy <yejunwen2@h-partners.com>
2024-06-04 07:46:51 +0000

    update test262/run_test262.py.
    Signed-off-by: yy <yejunwen2@h-partners.com>

--- test262/run_test262.py
+++ test262/run_test262.py
@@ -124,7 +124,7 @@ def parse_args():
                         help="Run test262 with JIT")
     parser.add_argument('--abc2program', action='store_true',
                         help="Use abc2prog to generate abc, aot or pgo is not supported yet under this option")
-    parser.add_argument('--disenable-force-gc', action='store_true',
+    parser.add_argument('--disable-force-gc', action='store_true',
                         help="Run test262 with close force-gc")
 
     args = parser.parse_args()
@@ -622,8 +622,8 @@ def get_host_args_of_ark_arch(args, host_args):
 
     return host_args
 
-def get_disenable_force_gc(host_args, args):
-    host_args += f"--disenable-force-gc "
+def get_disable_force_gc(host_args, args):
+    host_args += f"--disable-force-gc "
 
     return host_args
 
@@ -682,8 +682,8 @@ def get_host_args(args, host_type):
     if args.ark_arch != ark_arch:
         host_args = get_host_args_of_ark_arch(args, host_args)
 
-    if args.disenable_force_gc:
-        host_args = get_disenable_force_gc(host_args, args)
+    if args.disable_force_gc:
+        host_args = get_disable_force_gc(host_args, args)
 
     return host_args
 
