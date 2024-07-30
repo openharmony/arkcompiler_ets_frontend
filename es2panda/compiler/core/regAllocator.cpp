86b2c91c7dd58983e5d53808a4e03130139f3b01
shixiaowei4 <shixiaowei4@huawei.com>
2024-07-30 17:11:14 +0800

    Fix IcSlot
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAL215?from=project-issue
    
    Signed-off-by: shixiaowei4 <shixiaowei4@huawei.com>
    Change-Id: Ib689e4aedb8b1892ee8a1b17803b7ab8b553de6c

--- es2panda/compiler/core/regAllocator.cpp
+++ es2panda/compiler/core/regAllocator.cpp
@@ -58,7 +58,8 @@ uint16_t RegAllocator::GetSpillRegsCount() const
 void RegAllocator::UpdateIcSlot(IRNode *node)
 {
     auto inc = node->SetIcSlot(pg_->GetCurrentSlot());
-    if (node->InlineCacheEnabled() && (inc == 0) && !pg_->IsIcOverFlow()) {
+    constexpr static ICSlot invalid = 0xFF;
+    if (node->InlineCacheEnabled() && (node->GetIcSlot() == invalid) && !pg_->IsIcOverFlow()) {
         pg_->SetIcOverFlow();
     }
     pg_->IncreaseCurrentSlot(inc);
