9542392cece67c02c898e35feab07b0395e96f2c
Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>
2024-08-08 15:59:45 +0300

    No CTE in Ensure-Not-Nullish Expression
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAITLP?from=project-issue
    
    Tests: Test-U-Runner and CI
    
    Signed-off-by: Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>

--- ets2panda/checker/ETSAnalyzer.cpp
+++ ets2panda/checker/ETSAnalyzer.cpp
@@ -2348,12 +2348,6 @@ checker::Type *ETSAnalyzer::Check(ir::TSNonNullExpression *expr) const
     if (expr->TsTypeOrError() == nullptr) {
         ETSChecker *checker = GetETSChecker();
         auto exprType = expr->expr_->Check(checker);
-        if (!exprType->PossiblyETSNullish()) {
-            checker->ThrowTypeError(
-                "Bad operand type, the operand of the non-nullish expression must be a nullish type",
-                expr->Expr()->Start());
-        }
-
         //  If the actual [smart] type is definitely 'null' or 'undefined' then probably CTE should be thrown.
         //  Anyway we'll definitely obtain NullPointerException at runtime.
         if (exprType->DefinitelyETSNullish()) {
@@ -2361,7 +2355,6 @@ checker::Type *ETSAnalyzer::Check(ir::TSNonNullExpression *expr) const
                 "Bad operand type, the operand of the non-nullish expression is 'null' or 'undefined'.",
                 expr->Expr()->Start());
         }
-
         expr->SetTsType(checker->GetNonNullishType(exprType));
     }
     expr->SetOriginalType(expr->TsType());
