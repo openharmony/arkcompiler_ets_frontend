78f5154e7f7d1e28ffee783b555342e51d78bed7
zhangkai366 <zhangkai366@h-partners.com>
2024-08-23 18:51:31 +0800

    add ut for ConfigResolver file
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAM4QE
    Test: grammar & ut
    
    Signed-off-by: zhangkai366 <zhangkai366@h-partners.com>
    Change-Id: I0f2c62abe4bde41wa2af0e5867b5c8d8a5aff45m

--- arkguard/test/ut/initialization/ConfigResolver.spec.ts
+++ arkguard/test/ut/initialization/ConfigResolver.spec.ts
@@ -704,155 +704,163 @@ describe('test for ConfigResolve', function() {
 
     describe('1: test Api getSystemApiCache', function() {
       it('1-1: test getSystemApiCache: -enable-property-obfuscation', function () {
-         let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'property');
-         let obfuscationOptions = {
-           'selfConfig': {
-             'ruleOptions': {
-               'enable': true,
-               'rules': [ 
-                 path.join(OBFUSCATE_TESTDATA_DIR, 'property/property.txt')
-               ]
-             },
-             'consumerRules': [],
-           },
-           'dependencies': {
-             'libraries': [],
-             'hars': []
-           },
-           'obfuscationCacheDir': obfuscationCacheDir,
-           'sdkApis': [
-             path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
-           ]
-         };
-         let projectConfig = {
-           obfuscationOptions,
-           compileHar: false
-         };
-         const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
-         obConfig.resolveObfuscationConfigs();
-         const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
-         const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
+        let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'property');
+        let obfuscationOptions = {
+          'selfConfig': {
+            'ruleOptions': {
+              'enable': true,
+              'rules': [ 
+                path.join(OBFUSCATE_TESTDATA_DIR, 'property/property.txt')
+              ]
+            },
+            'consumerRules': [],
+          },
+          'dependencies': {
+            'libraries': [],
+            'hars': []
+          },
+          'obfuscationCacheDir': obfuscationCacheDir,
+          'sdkApis': [
+            path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
+          ]
+        };
+        let projectConfig = {
+          obfuscationOptions,
+          compileHar: false
+        };
+        const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
+        obConfig.resolveObfuscationConfigs();
+        const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
+        const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
+    
+        expect(reservedSdkApiForProp.size == 12).to.be.true;
+        expect(reservedSdkApiForProp.has('TestClass')).to.be.true;
+        expect(reservedSdkApiForProp.has('para1')).to.be.true;
+        expect(reservedSdkApiForProp.has('para2')).to.be.true;
+        expect(reservedSdkApiForProp.has('foo')).to.be.true;
+        expect(reservedSdkApiForProp.has('TestFunction')).to.be.true;
+        expect(reservedSdkApiForProp.has('funcPara1')).to.be.true;
+        expect(reservedSdkApiForProp.has('funcPara2')).to.be.true;
+        expect(reservedSdkApiForProp.has('ns')).to.be.true;
+        expect(reservedSdkApiForProp.has('NavigationBuilderRegister')).to.be.true;
+        expect(reservedSdkApiForProp.has('ViewV2')).to.be.true;
+        expect(reservedSdkApiForProp.has('initParam')).to.be.true;
+        expect(reservedSdkApiForProp.has('updateParam')).to.be.true;
+        expect(reservedSdkApiForGlobal.size == 0).to.be.true;
+        UnobfuscationCollections.clear();
+    
+        let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
+        const data = fs.readFileSync(systemApiPath, 'utf8');
+        const systemApiContent = JSON.parse(data);
+    
+        expect(systemApiContent.ReservedPropertyNames.length == 12).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('TestClass')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('para1')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('para2')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('foo')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('TestFunction')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('funcPara1')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('funcPara2')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('ns')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('NavigationBuilderRegister')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('ViewV2')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('initParam')).to.be.true;
+        expect(systemApiContent.ReservedPropertyNames.includes('updateParam')).to.be.true;
+        expect(systemApiContent.ReservedGlobalNames == undefined).to.be.true;
+    
+        fs.unlinkSync(systemApiPath);
+      });
      
-         expect(reservedSdkApiForProp.size == 8).to.be.true;
-         expect(reservedSdkApiForProp.has('TestClass')).to.be.true;
-         expect(reservedSdkApiForProp.has('para1')).to.be.true;
-         expect(reservedSdkApiForProp.has('para2')).to.be.true;
-         expect(reservedSdkApiForProp.has('foo')).to.be.true;
-         expect(reservedSdkApiForProp.has('TestFunction')).to.be.true;
-         expect(reservedSdkApiForProp.has('funcPara1')).to.be.true;
-         expect(reservedSdkApiForProp.has('funcPara2')).to.be.true;
-         expect(reservedSdkApiForProp.has('ns')).to.be.true;
-         expect(reservedSdkApiForGlobal.size == 0).to.be.true;
-         UnobfuscationCollections.clear();
+      it('1-2: test getSystemApiCache: -enable-export-obfuscation', function () {
+        let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'export');
+        let obfuscationOptions = {
+          'selfConfig': {
+            'ruleOptions': {
+              'enable': true,
+              'rules': [ 
+                path.join(OBFUSCATE_TESTDATA_DIR, 'export/export.txt')
+              ]
+            },
+            'consumerRules': [],
+          },
+          'dependencies': {
+            'libraries': [],
+            'hars': []
+          },
+          'obfuscationCacheDir': obfuscationCacheDir,
+          'sdkApis': [
+            path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
+          ]
+        };
+        let projectConfig = {
+          obfuscationOptions,
+          compileHar: false
+        };
+        const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
+        obConfig.resolveObfuscationConfigs();
+        const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
+        const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
+    
+        expect(reservedSdkApiForProp.size == 0).to.be.true;
+        expect(reservedSdkApiForGlobal.size == 0).to.be.true;
+        UnobfuscationCollections.clear();
+    
+        let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
+        const noSystemApi = fs.existsSync(systemApiPath);
+    
+        expect(noSystemApi).to.be.false;
+      });
      
-         let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
-         const data = fs.readFileSync(systemApiPath, 'utf8');
-         const systemApiContent = JSON.parse(data);
-     
-         expect(systemApiContent.ReservedPropertyNames.length == 8).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('TestClass')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('para1')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('para2')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('foo')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('TestFunction')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('funcPara1')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('funcPara2')).to.be.true;
-         expect(systemApiContent.ReservedPropertyNames.includes('ns')).to.be.true;
-         expect(systemApiContent.ReservedGlobalNames == undefined).to.be.true;
-     
-         fs.unlinkSync(systemApiPath);
-       });
-     
-       it('1-2: test getSystemApiCache: -enable-export-obfuscation', function () {
-         let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'export');
-         let obfuscationOptions = {
-           'selfConfig': {
-             'ruleOptions': {
-               'enable': true,
-               'rules': [ 
-                 path.join(OBFUSCATE_TESTDATA_DIR, 'export/export.txt')
-               ]
-             },
-             'consumerRules': [],
-           },
-           'dependencies': {
-             'libraries': [],
-             'hars': []
-           },
-           'obfuscationCacheDir': obfuscationCacheDir,
-           'sdkApis': [
-             path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
-           ]
-         };
-         let projectConfig = {
-           obfuscationOptions,
-           compileHar: false
-         };
-         const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
-         obConfig.resolveObfuscationConfigs();
-         const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
-         const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
-     
-         expect(reservedSdkApiForProp.size == 0).to.be.true;
-         expect(reservedSdkApiForGlobal.size == 0).to.be.true;
-         UnobfuscationCollections.clear();
-     
-         let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
-         const noSystemApi = fs.existsSync(systemApiPath);
-     
-         expect(noSystemApi).to.be.false;
-       });
-     
-       it('1-3: test getSystemApiCache: -enable-export-obfuscation -enable-toplevel-obfuscation', function () {
-         let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'export_toplevel');
-         let obfuscationOptions = {
-           'selfConfig': {
-             'ruleOptions': {
-               'enable': true,
-               'rules': [ 
-                 path.join(OBFUSCATE_TESTDATA_DIR, 'export_toplevel/export_toplevel.txt')
-               ]
-             },
-             'consumerRules': [],
-           },
-           'dependencies': {
-             'libraries': [],
-             'hars': []
-           },
-           'obfuscationCacheDir': obfuscationCacheDir,
-           'sdkApis': [
-             path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
-           ]
-         };
-         let projectConfig = {
-           obfuscationOptions,
-           compileHar: false
-         };
-         const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
-         obConfig.resolveObfuscationConfigs();
-         const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
-         const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
-     
-         expect(reservedSdkApiForProp.size == 0).to.be.true;
-         expect(reservedSdkApiForGlobal.size == 3).to.be.true;
-         expect(reservedSdkApiForGlobal.has('TestClass')).to.be.true;
-         expect(reservedSdkApiForGlobal.has('TestFunction')).to.be.true;
-         expect(reservedSdkApiForGlobal.has('ns')).to.be.true;
-         UnobfuscationCollections.clear();
-     
-         let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
-         const data = fs.readFileSync(systemApiPath, 'utf8');
-         const systemApiContent = JSON.parse(data);
-     
-         expect(systemApiContent.ReservedPropertyNames == undefined).to.be.true;
-         expect(systemApiContent.ReservedGlobalNames.length == 3).to.be.true;
-         expect(systemApiContent.ReservedGlobalNames.includes('TestClass')).to.be.true;
-         expect(systemApiContent.ReservedGlobalNames.includes('TestFunction')).to.be.true;
-         expect(systemApiContent.ReservedGlobalNames.includes('ns')).to.be.true;
-     
-         fs.unlinkSync(systemApiPath);
-       });
-     })
+      it('1-3: test getSystemApiCache: -enable-export-obfuscation -enable-toplevel-obfuscation', function () {
+        let obfuscationCacheDir = path.join(OBFUSCATE_TESTDATA_DIR, 'export_toplevel');
+        let obfuscationOptions = {
+          'selfConfig': {
+            'ruleOptions': {
+              'enable': true,
+              'rules': [ 
+                path.join(OBFUSCATE_TESTDATA_DIR, 'export_toplevel/export_toplevel.txt')
+              ]
+            },
+            'consumerRules': [],
+          },
+          'dependencies': {
+            'libraries': [],
+            'hars': []
+          },
+          'obfuscationCacheDir': obfuscationCacheDir,
+          'sdkApis': [
+            path.join(OBFUSCATE_TESTDATA_DIR, 'system_api.d.ts')
+          ]
+        };
+        let projectConfig = {
+          obfuscationOptions,
+          compileHar: false
+        };
+        const obConfig: ObConfigResolver =  new ObConfigResolver(projectConfig, undefined);
+        obConfig.resolveObfuscationConfigs();
+        const reservedSdkApiForProp = UnobfuscationCollections.reservedSdkApiForProp;
+        const reservedSdkApiForGlobal = UnobfuscationCollections.reservedSdkApiForGlobal;
+    
+        expect(reservedSdkApiForProp.size == 0).to.be.true;
+        expect(reservedSdkApiForGlobal.size == 3).to.be.true;
+        expect(reservedSdkApiForGlobal.has('TestClass')).to.be.true;
+        expect(reservedSdkApiForGlobal.has('TestFunction')).to.be.true;
+        expect(reservedSdkApiForGlobal.has('ns')).to.be.true;
+        UnobfuscationCollections.clear();
+    
+        let systemApiPath = obfuscationCacheDir + '/systemApiCache.json';
+        const data = fs.readFileSync(systemApiPath, 'utf8');
+        const systemApiContent = JSON.parse(data);
+    
+        expect(systemApiContent.ReservedPropertyNames == undefined).to.be.true;
+        expect(systemApiContent.ReservedGlobalNames.length == 3).to.be.true;
+        expect(systemApiContent.ReservedGlobalNames.includes('TestClass')).to.be.true;
+        expect(systemApiContent.ReservedGlobalNames.includes('TestFunction')).to.be.true;
+        expect(systemApiContent.ReservedGlobalNames.includes('ns')).to.be.true;
+    
+        fs.unlinkSync(systemApiPath);
+      });
+    });
   });
   
   describe('collectResevedFileNameInIDEConfig', () => {
