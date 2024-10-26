24b81392bbe8f0aa84862a0a58dd0bb7a6a6ea28
zengzengran <zengran2@huawei.com>
2024-10-26 12:04:27 +0800

    Modify the returntype assignability logic
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IAZXOP
    
    Description:
    When the returntype of override is primitivetype, false is returned when source returntype and target returntype are different. Because primitivetype has no returntypeannotation, obtaining the returntypeannotation directly results in a segment fault.
    
    When the returntype of override is object, and source returntype and target returntype cannot match, the system checks whether target is typeparameter. If no, false is returned. Getting the returntypeannotation directly causes the segment fault.
    
    Tested-by:
    ninja tests (passed)
    ets_testrunner (passed)
    
    Signed-off-by: zengzengran <zengran2@huawei.com>##

--- ets2panda/checker/ets/function.cpp
+++ ets2panda/checker/ets/function.cpp
@@ -1761,7 +1761,8 @@ bool ETSChecker::IsReturnTypeSubstitutable(Signature *const s1, Signature *const
 
     // - If R1 is a primitive type then R2 is identical to R1.
     if (r1->HasTypeFlag(TypeFlag::ETS_PRIMITIVE | TypeFlag::ETS_INT_ENUM | TypeFlag::ETS_STRING_ENUM |
-                        TypeFlag::ETS_VOID)) {
+                        TypeFlag::ETS_VOID) ||
+        r2->HasTypeFlag(TypeFlag::ETS_PRIMITIVE | TypeFlag::ETS_INT_ENUM | TypeFlag::ETS_STRING_ENUM)) {
         return Relation()->IsIdenticalTo(r2, r1);
     }
 
@@ -1774,6 +1775,7 @@ bool ETSChecker::IsReturnTypeSubstitutable(Signature *const s1, Signature *const
     }
 
     return s2->Function()->ReturnTypeAnnotation()->IsETSTypeReference() &&
+           s2->Function()->ReturnTypeAnnotation()->GetType(this)->IsETSTypeParameter() &&
            Relation()->IsSupertypeOf(
                s2->Function()->ReturnTypeAnnotation()->GetType(this)->AsETSTypeParameter()->GetConstraintType(), r1);
 }
