953def09c7b83f81e708d0f871624c508d7e7d6e
huyunhui1 <huyunhui1@huawei.com>
2024-11-04 19:55:08 +0800

    Modify default api version
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IB20GL
    
    Signed-off-by: huyunhui1 <huyunhui1@huawei.com>
    Change-Id: I580b5de25acef3b01ef49ea92b77b41eb1c5591a

--- es2panda/aot/options.cpp
+++ es2panda/aot/options.cpp
@@ -429,8 +429,8 @@ bool Options::Parse(int argc, const char **argv)
         "bc-min-version are enabled, only bc-version will take effects");
     panda::PandArg<bool> bcMinVersion("bc-min-version", false, "Print ark bytecode minimum supported version");
     // todo(huyunhui): change default api verion to 0 after refactoring
-    // The latest api version in isa.yaml is 15
-    panda::PandArg<int> targetApiVersion("target-api-version", 15,
+    // Current api version is 14
+    panda::PandArg<int> targetApiVersion("target-api-version", 14,
         "Specify the targeting api version for es2abc to generated the corresponding version of bytecode");
     panda::PandArg<bool> targetBcVersion("target-bc-version", false, "Print the corresponding ark bytecode version"\
         "for target api version. If both target-bc-version and bc-version are enabled, only target-bc-version"\
