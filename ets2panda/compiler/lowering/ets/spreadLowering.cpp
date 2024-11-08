6f0f61ee9f3709fd058d419f95743c84815b0127
xingshunxiang <xingshunxiang@huawei.com>
2024-11-08 20:30:27 +0800

    Fix redefinition of internal and user variables
    Function GenName is uesd to generate some internal temporary variableID with string gensymets2panda/ in compiler, but if users use the string to define some variable too, it leads to redefinition.
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IB1YGI?from=project-issue
    
    Signed-off-by: xingshunxiang <xingshunxiang@huawei.com>

--- ets2panda/compiler/lowering/ets/spreadLowering.cpp
+++ ets2panda/compiler/lowering/ets/spreadLowering.cpp
@@ -23,103 +23,210 @@ namespace ark::es2panda::compiler {
 
 using AstNodePtr = ir::AstNode *;
 
-std::string SpreadConstructionPhase::CreateLengthString(ir::ArrayExpression *array,
-                                                        const std::string_view spreadArrayName,
-                                                        const std::string_view lengthOfNewArray)
+static ir::AstNode *SetSourceRangesRecursively(ir::AstNode *node)
 {
-    int spreadElementCount = 0;
-    std::stringstream lengthCalculationString;
+    auto const refine = [](ir::AstNode *n) { n->SetRange(n->Parent()->Range()); };
 
-    for (std::size_t i = 0; i < array->Elements().size(); ++i) {
-        if (array->Elements()[i]->Type() == ir::AstNodeType::SPREAD_ELEMENT) {
-            spreadElementCount++;
-            lengthCalculationString << spreadArrayName << "_" << i << ".length + ";
-        }
-    }
-
-    lengthCalculationString << "0";
-    int newArrayLength = array->Elements().size() - spreadElementCount;
-    std::stringstream lengthString;
-    lengthString << "let " << lengthOfNewArray << " : int = " << newArrayLength << " + "
-                 << lengthCalculationString.str() << std::endl;
-
-    return lengthString.str();
+    refine(node);
+    node->IterateRecursively(refine);
+    return node;
 }
 
-/*
- * NOTE: Create sourceCode to expand the SpreadExpression, which is as follows :
- * let spreadArrayName_0 = (@@E1)
- * let length : int = 0 + spreadArrayName_0.length + 0
- * type typeOfNewArray = arrayType
- * let tempArrayVar: typeOfNewArray[] = new typeOfNewArray[length]
- * let newArrayIndex = 0
- * let elementOfSpread_0: arrayType
- * for (elementOfSpread_0 of spreadArrayName_0) {
- *     tempArrayVar[newArrayIndex] = elementOfSpread_0
- *     newArrayIndex++
- * }
- * tempArrayVar[newArrayIndex] = (@@E2)
- * newArrayIndex++
- * ...
- * tempArrayVar;
- */
-std::string SpreadConstructionPhase::CreateETSCode(ir::ArrayExpression *array, std::vector<ir::AstNode *> &node,
-                                                   public_lib::Context *ctx)
+void CreateSpreadArrayDeclareStatements(public_lib::Context *ctx, ir::ArrayExpression *array,
+                                        std::vector<ir::Identifier *> &spreadArrayIds,
+                                        ArenaVector<ir::Statement *> &statements)
 {
-    const util::StringView &spreadArrayName = GenName(ctx->checker->Allocator()).View();
-    const util::StringView &newArrayName = GenName(ctx->checker->Allocator()).View();
-    const util::StringView &newArrayIndex = GenName(ctx->checker->Allocator()).View();
-    const util::StringView &typeOfNewArray = GenName(ctx->checker->Allocator()).View();
-    const util::StringView &elementOfSpread = GenName(ctx->checker->Allocator()).View();
-    const util::StringView &lengthOfNewArray = GenName(ctx->checker->Allocator()).View();
-
-    std::string lengthString = CreateLengthString(array, spreadArrayName.Utf8(), lengthOfNewArray.Utf8());
-    std::string arrayType = array->TsType()->AsETSArrayType()->ElementType()->ToString();
-
-    std::stringstream src;
-    src.clear();
-    size_t argumentCount = 1;
-
-    for (std::size_t i = 0; i < array->Elements().size(); ++i) {
-        if (array->Elements()[i]->Type() != ir::AstNodeType::SPREAD_ELEMENT) {
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    for (auto element : array->Elements()) {
+        if (element->Type() != ir::AstNodeType::SPREAD_ELEMENT) {
             continue;
         }
-        src << "let " << spreadArrayName << "_" << i << " = (@@E" << argumentCount << ")" << std::endl;
-        argumentCount++;
-        node.emplace_back(array->Elements()[i]->AsSpreadElement()->Argument()->Clone(ctx->allocator, nullptr));
+        ir::Identifier *const arrIdent = Gensym(allocator);
+        auto *const initExpr = element->AsSpreadElement()->Argument()->Clone(allocator, nullptr);
+        spreadArrayIds.emplace_back(arrIdent);
+        statements.emplace_back(parser->CreateFormattedStatement("let @@I1 = (@@E2);", arrIdent, initExpr));
+    }
+}
+
+void CreateNewArrayLengthStatement(public_lib::Context *ctx, ir::ArrayExpression *array,
+                                   std::vector<ir::Identifier *> &spreadArrayIds,
+                                   ArenaVector<ir::Statement *> &statements, ir::Identifier *newArrayLengthId)
+{
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    std::vector<ir::AstNode *> nodesWaitingInsert {newArrayLengthId};
+    int argumentCount = 1;
+    std::stringstream lengthString;
+    int normalElementCount = array->Elements().size() - spreadArrayIds.size();
+    lengthString.clear();
+    lengthString << "let @@I" << (argumentCount++) << " : int = " << normalElementCount << " + ";
+    for (auto *spaId : spreadArrayIds) {
+        lengthString << "@@I" << (argumentCount++) << ".length + ";
+        nodesWaitingInsert.emplace_back(spaId->Clone(allocator, nullptr));
+    }
+    lengthString << "0;" << std::endl;
+    statements.emplace_back(parser->CreateFormattedStatement(lengthString.str(), nodesWaitingInsert));
+}
+
+ir::Identifier *ClearIdentifierAnnotation(ir::Identifier *node)
+{
+    if (!node->IsAnnotatedExpression() || (node->AsAnnotatedExpression()->TypeAnnotation() == nullptr)) {
+        return node;
     }
 
-    src << lengthString << std::endl;
+    node->AsAnnotatedExpression()->SetTsTypeAnnotation(nullptr);
+    return node;
+}
 
+ir::Identifier *CreateNewArrayDeclareStatement(public_lib::Context *ctx, ir::ArrayExpression *array,
+                                               ArenaVector<ir::Statement *> &statements,
+                                               ir::Identifier *newArrayLengthId)
+{
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    ir::Identifier *newArrayId = Gensym(allocator);
+    std::string arrayType = array->TsType()->AsETSArrayType()->ElementType()->ToString();
+    std::stringstream newArrayDeclareStr;
+    std::stringstream typeOfNewArray;
     // NOTE: For ETSUnionType(String|Int) or ETSObjectType(private constructor) or ..., we canot use "new Type[]" to
     //       declare an array, so we add "|null" to solve it temporarily.
     //       We might need to use cast Expression in the end of the generated source code to remove "|null", such as
     //       "newArrayName as arrayType[]".
     //       But now cast Expression doesn't support built-in array (cast fatherType[] to sonType[]), so "newArrayName
     //       as arrayType" should be added after cast Expression is implemented completely.
-    src << "type " << typeOfNewArray << " = " << arrayType << "|null" << std::endl;
-    src << "let " << newArrayName << ": " << typeOfNewArray << "[] = "
-        << "new " << typeOfNewArray << "[" << lengthOfNewArray << "]" << std::endl;
-    src << "let " << newArrayIndex << " = 0" << std::endl;
+    newArrayDeclareStr.clear();
+    typeOfNewArray.clear();
+    typeOfNewArray << "(" << arrayType << "|null)";
+    newArrayDeclareStr << "let @@I1: " << typeOfNewArray.str() << "[] = new " << typeOfNewArray.str() << "[@@I2];"
+                       << std::endl;
 
-    for (std::size_t i = 0; i < array->Elements().size(); ++i) {
-        if (array->Elements()[i]->Type() == ir::AstNodeType::SPREAD_ELEMENT) {
-            src << "let " << elementOfSpread << "_" << i << ": " << arrayType << std::endl;
-            src << "for (" << elementOfSpread << "_" << i << " of " << spreadArrayName << "_" << i << ") {"
-                << std::endl;
-            src << newArrayName << "[" << newArrayIndex << "] = " << elementOfSpread << "_" << i << std::endl;
-            src << newArrayIndex << "++" << std::endl;
-            src << "}" << std::endl;
+    ir::Statement *newArrayDeclareSt = parser->CreateFormattedStatement(
+        newArrayDeclareStr.str(), newArrayId, ClearIdentifierAnnotation(newArrayLengthId->Clone(allocator, nullptr)));
+    statements.emplace_back(newArrayDeclareSt);
+    return newArrayId;
+}
+
+ir::Statement *CreateSpreadArrIteratorStatement(public_lib::Context *ctx, ir::ArrayExpression *array,
+                                                ir::Identifier *spreadArrIterator)
+{
+    auto *const parser = ctx->parser->AsETSParser();
+    std::string arrayType = array->TsType()->AsETSArrayType()->ElementType()->ToString();
+
+    std::stringstream spArrIterDeclareStr;
+    spArrIterDeclareStr.clear();
+    spArrIterDeclareStr << "let @@I1: " << arrayType << ";" << std::endl;
+    ir::Statement *spArrIterDeclareSt = parser->CreateFormattedStatement(spArrIterDeclareStr.str(), spreadArrIterator);
+
+    return spArrIterDeclareSt;
+}
+
+ir::Statement *CreateElementsAssignStatementBySpreadArr(public_lib::Context *ctx, ir::Identifier *spId,
+                                                        std::vector<ir::AstNode *> &newArrayAndIndex,
+                                                        ir::Identifier *spreadArrIterator)
+{
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    auto *const newArrayId = newArrayAndIndex[0];
+    auto *const newArrayIndexId = newArrayAndIndex[1];
+
+    std::stringstream elementsAssignStr;
+    elementsAssignStr.clear();
+    elementsAssignStr << "for (@@I2 of @@I3) {" << std::endl;
+    elementsAssignStr << "@@I4[@@I5] = @@I6;" << std::endl;
+    elementsAssignStr << "@@I7++;" << std::endl;
+    elementsAssignStr << "}" << std::endl;
+
+    ir::Statement *elementsAssignStatement = parser->CreateFormattedStatement(
+        elementsAssignStr.str(), ClearIdentifierAnnotation(spreadArrIterator->Clone(allocator, nullptr)),
+        ClearIdentifierAnnotation(spreadArrIterator->Clone(allocator, nullptr)), spId->Clone(allocator, nullptr),
+        newArrayId->Clone(allocator, nullptr), newArrayIndexId->Clone(allocator, nullptr),
+        ClearIdentifierAnnotation(spreadArrIterator->Clone(allocator, nullptr)),
+        newArrayIndexId->Clone(allocator, nullptr));
+
+    return elementsAssignStatement;
+}
+
+ir::Statement *CreateElementsAssignStatementBySingle(public_lib::Context *ctx, ir::AstNode *element,
+                                                     std::vector<ir::AstNode *> &newArrayAndIndex)
+{
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    auto *const newArrayId = newArrayAndIndex[0];
+    auto *const newArrayIndexId = newArrayAndIndex[1];
+    std::stringstream elementsAssignStr;
+    elementsAssignStr.clear();
+    elementsAssignStr << "@@I1[@@I2] = (@@E3);" << std::endl;
+    elementsAssignStr << "@@I4++;" << std::endl;
+
+    ir::Statement *elementsAssignStatement = parser->CreateFormattedStatement(
+        elementsAssignStr.str(), newArrayId->Clone(allocator, nullptr), newArrayIndexId->Clone(allocator, nullptr),
+        element->Clone(allocator, nullptr), newArrayIndexId->Clone(allocator, nullptr));
+
+    return elementsAssignStatement;
+}
+
+void CreateNewArrayElementsAssignStatement(public_lib::Context *ctx, ir::ArrayExpression *array,
+                                           std::vector<ir::Identifier *> &spArrIds,
+                                           ArenaVector<ir::Statement *> &statements,
+                                           std::vector<ir::AstNode *> &newArrayAndIndex)
+{
+    auto *const allocator = ctx->allocator;
+    auto *const parser = ctx->parser->AsETSParser();
+    auto *const newArrayId = newArrayAndIndex[0];
+    int spArrIdx = 0;
+
+    for (auto *element : array->Elements()) {
+        if (element->Type() == ir::AstNodeType::SPREAD_ELEMENT) {
+            ir::Identifier *spreadArrIterator = Gensym(allocator);
+            statements.emplace_back(CreateSpreadArrIteratorStatement(ctx, array, spreadArrIterator));
+            statements.emplace_back(CreateElementsAssignStatementBySpreadArr(ctx, spArrIds[spArrIdx++],
+                                                                             newArrayAndIndex, spreadArrIterator));
         } else {
-            src << newArrayName << "[" << newArrayIndex << "] = (@@E" << argumentCount << ")" << std::endl;
-            src << newArrayIndex << "++" << std::endl;
-            argumentCount++;
-            node.emplace_back(array->Elements()[i]->Clone(ctx->allocator, nullptr));
+            statements.emplace_back(CreateElementsAssignStatementBySingle(ctx, element, newArrayAndIndex));
         }
     }
-    src << newArrayName << ";" << std::endl;
 
-    return src.str();
+    statements.emplace_back(parser->CreateFormattedStatement("@@I1;", newArrayId->Clone(allocator, nullptr)));
+}
+
+/*
+ * NOTE: Expand the SpreadExpr to BlockExpr, the rules as follows :
+ * let spreadArrayId1 = spreadExpr1
+ * let length : int = normalExprCount + spreadArrayId1.length + 0
+ * type typeOfNewArray = arrayType
+ * let newArray: typeOfNewArray[] = new typeOfNewArray[length]
+ * let newArrayIndex = 0
+ * let elementOfSpreadArray1: arrayType
+ * for (elementOfSpreadArray1 of spreadArray1) {
+ *     newArray[newArrayIndex] = elementOfSpreadArray1
+ *     newArrayIndex++
+ * }
+ * newArray[newArrayIndex] = normalExpr1
+ * newArrayIndex++
+ * ...
+ * newArray;
+ */
+ir::BlockExpression *CreateLoweredExpression(public_lib::Context *ctx, ir::ArrayExpression *array)
+{
+    auto *const checker = ctx->checker->AsETSChecker();
+    auto *const parser = ctx->parser->AsETSParser();
+    auto *const allocator = ctx->allocator;
+    ArenaVector<ir::Statement *> statements(allocator->Adapter());
+    std::vector<ir::Identifier *> spreadArrayIds = {};
+    ir::Identifier *newArrayLengthId = Gensym(allocator);
+
+    CreateSpreadArrayDeclareStatements(ctx, array, spreadArrayIds, statements);
+    CreateNewArrayLengthStatement(ctx, array, spreadArrayIds, statements, newArrayLengthId);
+
+    ir::Identifier *newArrayId = CreateNewArrayDeclareStatement(ctx, array, statements, newArrayLengthId);
+    ir::Identifier *newArrayIndexId = Gensym(allocator);
+    statements.emplace_back(parser->CreateFormattedStatement("let @@I1 = 0;", newArrayIndexId));
+    std::vector<ir::AstNode *> newArrayAndIndex {ClearIdentifierAnnotation(newArrayId->Clone(allocator, nullptr)),
+                                                 newArrayIndexId->Clone(allocator, nullptr)};
+
+    CreateNewArrayElementsAssignStatement(ctx, array, spreadArrayIds, statements, newArrayAndIndex);
+    return checker->AllocNode<ir::BlockExpression>(std::move(statements));
 }
 
 bool SpreadConstructionPhase::Perform(public_lib::Context *ctx, parser::Program *program)
@@ -131,27 +238,26 @@ bool SpreadConstructionPhase::Perform(public_lib::Context *ctx, parser::Program 
         }
     }
 
-    auto *const parser = ctx->parser->AsETSParser();
     checker::ETSChecker *const checker = ctx->checker->AsETSChecker();
 
     program->Ast()->TransformChildrenRecursively(
-        [&parser, &checker, &ctx, this](ir::AstNode *const node) -> AstNodePtr {
+        [&checker, &ctx](ir::AstNode *const node) -> AstNodePtr {
             if (node->IsArrayExpression() &&
                 std::any_of(node->AsArrayExpression()->Elements().begin(), node->AsArrayExpression()->Elements().end(),
                             [](const auto *param) { return param->Type() == ir::AstNodeType::SPREAD_ELEMENT; })) {
                 auto scopeCtx =
                     varbinder::LexicalScope<varbinder::Scope>::Enter(checker->VarBinder(), NearestScope(node));
-                std::vector<ir::AstNode *> normalElements {};
-                std::string src = CreateETSCode(node->AsArrayExpression(), normalElements, ctx);
 
-                ir::BlockExpression *blockExpression =
-                    parser->CreateFormattedExpression(src, normalElements)->AsBlockExpression();
+                ir::BlockExpression *blockExpression = CreateLoweredExpression(ctx, node->AsArrayExpression());
                 blockExpression->SetParent(node->Parent());
                 InitScopesPhaseETS::RunExternalNode(blockExpression, checker->VarBinder());
                 checker->VarBinder()->AsETSBinder()->ResolveReferencesForScope(blockExpression,
                                                                                NearestScope(blockExpression));
                 blockExpression->Check(checker);
 
+                // NOTE: this blockExpression is a kind of formatted-dummy code, which is invisible to users,
+                //       so, its source range should be same as the original code([element1, element2, ...spreadExpr])
+                SetSourceRangesRecursively(blockExpression);
                 return blockExpression;
             }
 
