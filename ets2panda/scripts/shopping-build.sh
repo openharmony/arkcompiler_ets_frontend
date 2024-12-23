be73b7c9ac1cc0c6d1e34f370732fe76e06baf8c
Tatiana Titova <titova.tatiana@huawei-partners.com>
2024-12-23 17:48:14 +0300

    Separate common part from Koala demos
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBDDW9
    Signed-off-by: Tatiana Titova <titova.tatiana@huawei-partners.com>

--- ets2panda/scripts/shopping-build.sh
+++ ets2panda/scripts/shopping-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2025 Huawei Device Co., Ltd.
+# Copyright (c) 2024-2025 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -17,76 +17,11 @@
 
 set -ex
 
-if [ -z "${NEXUS_REPO}" ]; then
-    echo "Please set NEXUS_REPO"
-    echo "export NEXUS_REPO=nexus.example.com:1234"
-    exit 1
-fi
-
-HUAWEI_MIRROR="${HUAWEI_MIRROR:-https://repo.huaweicloud.com/repository/npm/}"
-KOALA_REGISTRY="${KOALA_REGISTRY:-https://$NEXUS_REPO/repository/koala-npm/}"
-NPROC_PER_JOB="${NPROC_PER_JOB:-16}"
-
-function do_checkout() {
-    local repo=$1
-    local rev=$2
-    local dest=$3
-    local patch=$4
-    [ -n "${repo}" ] || exit 1
-    [ -n "${rev}" ] || exit 1
-    [ -n "${dest}" ] || exit 1
-    mkdir -p "${dest}"
-    pushd "${dest}" || exit 1
-        git init && git remote add origin "${repo}"
-        # NOTE(titova): add repeat
-        git fetch --depth 1 origin "${rev}" || {
-            echo "(Some error occurred while fetching rev: ${rev}"
-            exit 1
-        } && {
-            git checkout FETCH_HEAD || exit 1
-            [ -n "${patch}" ] && git apply "${patch}"
-        }
-    popd >/dev/null 2>&1 || exit 1
-}
-
-GIT_URL=https://gitee.com/openharmony-sig/arkcompiler_ets_frontend.git
-DEST=koala-sig
-do_checkout "${GIT_URL}" panda_rev_4_qualified_names "${DEST}"
-
-cd "${DEST}" || exit 1
-
-npm config set package-lock false
-npm config set strict-ssl false
-# npm config set lockfile false
-npm config set registry "${HUAWEI_MIRROR}"
-npm config set @koalaui:registry "${KOALA_REGISTRY}"
-npm config set @panda:registry "https://$NEXUS_REPO/repository/koala-npm/"
-npm config set @ohos:registry "https://repo.harmonyos.com/npm/"
-if [ -z "${KOALA_REPO}" ] ; then
-    npm config set "//$NEXUS_REPO/repository/koala-npm/:_auth=$KOALA_TOKEN"
-fi
-
-npm i
-
-
-pushd incremental/tools/fast-arktsc/ || exit
-npm i
-popd >/dev/null 2>&1 || exit 1
-
-pushd incremental/tools/panda/ || exit 1
-if [ -z "${PANDA_SDK_TARBALL}" ] ; then
-npm run panda:sdk:install
-else
-npm install "${PANDA_SDK_TARBALL}"
-fi
-popd >/dev/null 2>&1 || exit 1
-
-pushd arkoala/arkui-common/ || exit 1
-KOALA_BZ=1 npm run ohos-sdk
-popd >/dev/null 2>&1 || exit 1
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "${SCRIPT_DIR}"/arkui-setup.sh
 
 pushd arkoala-arkts || exit 1
-npm run shopping:all:node
+NINJA_OPTIONS="${NINJA_OPTIONS}" npm run shopping:all:node
 popd >/dev/null 2>&1 || exit 1
 
 exit 0
