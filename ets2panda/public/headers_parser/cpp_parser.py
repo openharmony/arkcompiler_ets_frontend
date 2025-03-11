29c977b6a620874a09025e98ed6fc2124a18979f
Zhelyapov Aleksey <zhelyapov.aleksey@huawei-partners.com>
2025-03-11 19:38:40 +0300

    Provide compiler options, arktsconfig plugin API
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBNQGE
    
    Signed-off-by: Zhelyapov Aleksey <zhelyapov.aleksey@huawei-partners.com>
    Change-Id: If5679163e149099e1e38f0f4242f3b33388267f5

--- ets2panda/public/headers_parser/cpp_parser.py
+++ ets2panda/public/headers_parser/cpp_parser.py
@@ -58,6 +58,10 @@ class CppParser:
             elif self.it.is_template():
                 self.it.end, self.template = parse_template_prefix(self.it.data, self.it.start)
 
+            elif self.it.is_using():
+                self.it.end, self.parsed = parse_using(self.it.data, self.it.start)
+                self.res_append_in_modifier("usings")
+
             elif self.it.is_namespace():
                 self.it.end, self.parsed = parse_namespace(self.it.data, self.it.start)
                 self.res_update()
@@ -69,10 +73,6 @@ class CppParser:
             elif self.it.is_struct():
                 self.it.end, self.parsed = parse_struct(self.it.data, self.it.start)
                 self.res_append("structs")
-
-            elif self.it.is_using():
-                self.it.end, self.parsed = parse_using(self.it.data, self.it.start)
-                self.res_append_in_modifier("usings")
 
             elif self.it.is_define_macro():
                 self.it.end, self.parsed = parse_define_macros(self.it.data, self.it.start)
@@ -130,10 +130,10 @@ class CppParser:
                 return
             raise RuntimeError("Unreachable")
 
-        if key not in self.res[self.current_modifier]: # CC-OFF(G.TYP.07) dict key exist
-            self.res[self.current_modifier][key] = [] # CC-OFF(G.TYP.07) dict key exist
+        if key not in self.res[self.current_modifier]:  # CC-OFF(G.TYP.07) dict key exist
+            self.res[self.current_modifier][key] = []  # CC-OFF(G.TYP.07) dict key exist
 
-        self.res[self.current_modifier][key].append(deep_copy(self.parsed)) # CC-OFF(G.TYP.07) dict key exist
+        self.res[self.current_modifier][key].append(deep_copy(self.parsed))  # CC-OFF(G.TYP.07) dict key exist
 
     def res_update(self) -> None:
         if self.parsed:
@@ -165,11 +165,11 @@ class CppParser:
             return
 
         # Constructor
-        if self.parsed["name"] == self.parent_class_name: # CC-OFF(G.TYP.07) dict key exist
+        if self.parsed["name"] == self.parent_class_name:  # CC-OFF(G.TYP.07) dict key exist
             self.res_append_in_modifier("constructors")
 
         # Destructor
-        elif self.parsed["name"] == "~" + self.parent_class_name: # CC-OFF(G.TYP.07) dict key exist
+        elif self.parsed["name"] == "~" + self.parent_class_name:  # CC-OFF(G.TYP.07) dict key exist
             self.res_append_in_modifier("destructors")
 
         # Method
