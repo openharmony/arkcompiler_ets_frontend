8606afb5f230fd3360627885c6ceb69bf3a66cf7
zengzengran <zengran2@huawei.com>
2025-03-17 16:59:33 +0800

    Implement spec3.19.3 keyof types
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBSLNR
    
    Description:
    Need to implement the keyof keyword of spec3.19.3, A special form of union types are keyof types built by using the keyword keyof.The keyword keyof is applied to the class or interface type.
    
    Tested-by:
    ninja tests (passed)
    ets_testrunner (passed)
    
    Signed-off-by: zengzengran <zengran2@huawei.com>#

--- ets2panda/compiler/core/ETSCompilerUnrechable.cpp
+++ ets2panda/compiler/core/ETSCompilerUnrechable.cpp
@@ -142,6 +142,11 @@ void ETSCompiler::Compile([[maybe_unused]] const ir::ETSUnionType *node) const
     ES2PANDA_UNREACHABLE();
 }
 
+void ETSCompiler::Compile([[maybe_unused]] const ir::ETSKeyofType *node) const
+{
+    ES2PANDA_UNREACHABLE();
+}
+
 void ETSCompiler::Compile([[maybe_unused]] const ir::ArrowFunctionExpression *expr) const
 {
     // Should have been handled by LambdaLowering
--- ets2panda/ir/astNodeMapping.h
+++ ets2panda/ir/astNodeMapping.h
@@ -93,6 +93,7 @@
     _(ETS_TYPE_REFERENCE, ETSTypeReference)                                               \
     _(ETS_TYPE_REFERENCE_PART, ETSTypeReferencePart)                                      \
     _(ETS_UNION_TYPE, ETSUnionType)                                                       \
+    _(ETS_KEYOF_TYPE, ETSKeyofType)                                                       \
     _(ETS_LAUNCH_EXPRESSION, ETSLaunchExpression)                                         \
     _(ETS_NEW_ARRAY_INSTANCE_EXPRESSION, ETSNewArrayInstanceExpression)                   \
     _(ETS_NEW_MULTI_DIM_ARRAY_INSTANCE_EXPRESSION, ETSNewMultiDimArrayInstanceExpression) \
