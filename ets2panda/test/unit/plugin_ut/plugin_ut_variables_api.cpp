ed76ade99b84e5738cb5f708f185b6912f2acfec
tianlongdev <tianlong18@h-partners.com>
2025-03-21 18:23:46 +0800

    Add Expression CAPI test cases
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBTFN7
    
    Reason:
        Build CAPI protection
    
    Description:
        Add Expression CAPI test cases aimed to build CAPI protection
    
    Signed-off-by: tianlongdev <tianlong18@h-partners.com>

--- ets2panda/test/unit/plugin_ut/plugin_ut_variables_api.cpp
+++ ets2panda/test/unit/plugin_ut/plugin_ut_variables_api.cpp
@@ -15,67 +15,108 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstring>
 #include <iostream>
+#include <iterator>
 #include <ostream>
 #include <string>
 #include "../plugin/util.h"
 #include "public/es2panda_lib.h"
 
 // NOLINTBEGIN
-
-/**
+/*
  * Covered C API List:
- *   es2panda_AstNode **(*VariableDeclarationDeclaratorsConst)(es2panda_Context *context,
- *                                                             es2panda_AstNode *classInstance, size_t *returnTypeLen);
- *   es2panda_AstNode *(*VariableDeclaratorInit)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode *(*VariableDeclaratorId)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode *(*UpdateVariableDeclaration)(es2panda_Context *context, es2panda_AstNode *original,
- *                      Es2pandaVariableDeclarationKind kind, es2panda_AstNode **declarators, size_t declaratorsLen);
- *   bool (*IsVariableDeclaration)(es2panda_AstNode *ast);
- *   void (*AstNodeIterateConst)(es2panda_Context *context, es2panda_AstNode *classInstance, NodeTraverser cb);
- *   bool (*IsMethodDefinition)(es2panda_AstNode *ast);
- *   es2panda_AstNode *(*MethodDefinitionFunction)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   char *(*IdentifierName)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode *(*ScriptFunctionId)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   void (*AstNodeSetParent)(es2panda_Context *context, es2panda_AstNode *classInstance, es2panda_AstNode *parent);
- *   char *(*AstNodeDumpJSONConst)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode *(*ScriptFunctionBody)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode *(*CreateIdentifier1)(es2panda_Context *context, char *name);
- *   es2panda_AstNode *(*CreateVariableDeclarator1)(es2panda_Context *context, Es2pandaVariableDeclaratorFlag flag,
- *                                                  es2panda_AstNode *ident, es2panda_AstNode *init);
- *   es2panda_AstNode *(*VariableDeclaratorInit)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_AstNode **(*VariableDeclarationDeclaratorsConst)(es2panda_Context *context,
- *                                                             es2panda_AstNode *classInstance, size_t *returnTypeLen);
- *   void (*BlockStatementSetStatements)(es2panda_Context *context, es2panda_AstNode *classInstance,
- *                                       es2panda_AstNode **statementList, size_t statementListLen);
- *   void (*AstNodeForEach)(es2panda_AstNode *ast, void (*func)(es2panda_AstNode *, void *), void *arg);
- *   es2panda_Scope *(*AstNodeRebind)(es2panda_Context *ctx, es2panda_AstNode *node);
- *   void *(*AllocMemory)(es2panda_Context *context, size_t numberOfElements, size_t sizeOfElement);
- *   es2panda_AstNode *(*ProgramAst)(es2panda_Program *program);
- *   es2panda_Program *(*ContextProgram)(es2panda_Context *context);
- *   char *(*AstNodeDumpEtsSrcConst)(es2panda_Context *context, es2panda_AstNode *classInstance);
- *   es2panda_Config *(*CreateConfig)(int argc, char const *const *argv);
- *   es2panda_Context *(*CreateContextFromString)(es2panda_Config *config, const char *source, char const *file_name);
- *   es2panda_Context *(*ProceedToState)(es2panda_Context *context, es2panda_ContextState state);
- *   es2panda_ContextState (*ContextState)(es2panda_Context *context);
- *   void (*DestroyConfig)(es2panda_Config *config);
- *
+ *   AstNodeDumpEtsSrcConst
+ *   AstNodeDumpJSONConst
+ *   AstNodeForEach
+ *   AstNodeIterateConst
+ *   AstNodeRebind
+ *   AstNodeSetParent
+ *   BlockStatementSetStatements
+ *   ContextErrorMessage
+ *   ContextProgram
+ *   ContextState
+ *   CreateConfig
+ *   CreateContextFromString
+ *   CreateIdentifier1
+ *   CreateVariableDeclarator1
+ *   DestroyConfig
+ *   DestroyContext
+ *   FunctionDeclarationFunction
+ *   IdentifierName
+ *   IsFunctionDeclaration
+ *   IsVariableDeclaration
+ *   IsVariableDeclarator
+ *   ProceedToState
+ *   ProgramAst
+ *   ScriptFunctionBody
+ *   ScriptFunctionId
+ *   UpdateVariableDeclaration
+ *   VariableDeclarationDeclaratorsConst
+ *   VariableDeclaratorId
+ *   VariableDeclaratorIdConst
+ *   VariableDeclaratorInit
  */
-static es2panda_Impl *impl = nullptr;
 
+static std::string expectResStr(R"(
+{
+  "type": "Program",
+  "statements": [
+    {
+      "type": "FunctionDeclaration",
+      "function": {
+        "type": "ScriptFunction",
+        "id": {
+          "type": "Identifier",
+          "name": "main",
+          "decorators": []
+        },
+        "generator": false,
+        "async": false,
+        "expression": false,
+        "params": [],
+        "body": {
+          "type": "BlockStatement",
+          "statements": [
+            {
+              "type": "VariableDeclaration",
+              "declarations": [
+                {
+                  "type": "VariableDeclarator",
+                  "id": {
+                    "type": "Identifier",
+                    "name": "newVar",
+                    "decorators": []
+                  },
+                  "init": {
+                    "type": "NumberLiteral",
+                    "value": 5
+                  }
+                }
+              ],
+              "kind": "let"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
+)");
+static es2panda_Impl *g_impl = nullptr;
 static auto source = std::string("function main() { \nlet a = 5;\n }");
 
 static es2panda_AstNode *letStatement = nullptr;
 static es2panda_AstNode *mainScriptFunc = nullptr;
-static es2panda_Context *ctx = nullptr;
+static es2panda_Context *g_ctx = nullptr;
 
 es2panda_AstNode *parNode = nullptr;
 es2panda_Context *newCtx = nullptr;
 
 static void FindLet(es2panda_AstNode *ast)
 {
-    if (!impl->IsVariableDeclaration(ast)) {
-        impl->AstNodeIterateConst(ctx, ast, FindLet);
+    if (!g_impl->IsVariableDeclaration(ast)) {
+        g_impl->AstNodeIterateConst(g_ctx, ast, FindLet);
         return;
     }
     letStatement = ast;
@@ -83,13 +124,13 @@ static void FindLet(es2panda_AstNode *ast)
 
 static void FindMainDef(es2panda_AstNode *ast)
 {
-    if (!impl->IsMethodDefinition(ast)) {
-        impl->AstNodeIterateConst(ctx, ast, FindMainDef);
+    if (!g_impl->IsFunctionDeclaration(ast)) {
+        g_impl->AstNodeIterateConst(g_ctx, ast, FindMainDef);
         return;
     }
-    auto scriptFunc = impl->MethodDefinitionFunction(ctx, ast);
+    auto scriptFunc = g_impl->FunctionDeclarationFunction(g_ctx, ast);
     if (scriptFunc == nullptr ||
-        std::string("main") != impl->IdentifierName(ctx, impl->ScriptFunctionId(ctx, scriptFunc))) {
+        std::string("main") != g_impl->IdentifierName(g_ctx, g_impl->ScriptFunctionId(g_ctx, scriptFunc))) {
         return;
     }
     mainScriptFunc = scriptFunc;
@@ -97,7 +138,7 @@ static void FindMainDef(es2panda_AstNode *ast)
 
 static void changeParent(es2panda_AstNode *child)
 {
-    impl->AstNodeSetParent(newCtx, child, parNode);
+    g_impl->AstNodeSetParent(newCtx, child, parNode);
 }
 
 static void SetRightParent(es2panda_AstNode *node, void *arg)
@@ -105,65 +146,111 @@ static void SetRightParent(es2panda_AstNode *node, void *arg)
     es2panda_Context *context = static_cast<es2panda_Context *>(arg);
     newCtx = context;
     parNode = node;
-    impl->AstNodeIterateConst(context, node, changeParent);
+    g_impl->AstNodeIterateConst(context, node, changeParent);
 }
 
 /*
- *   "function main() { \nlet a = 5;\n"
- *   is changed to
- *   "function main() { \nlet newVar = 5;\n"
+ *   update variableDeclaration name
  */
-static bool VariableHandle(es2panda_Context *context)
+static bool UpdateAst(es2panda_Context *context, es2panda_AstNode *ast)
 {
-    auto Ast = impl->ProgramAst(context, impl->ContextProgram(context));
-    std::cout << impl->AstNodeDumpJSONConst(context, Ast) << std::endl;
-    ctx = context;
+    auto mainFuncBody = g_impl->ScriptFunctionBody(context, mainScriptFunc);
+    size_t n = 0;
+    es2panda_AstNode *varIdent = g_impl->CreateIdentifier1(context, const_cast<char *>("newVar"));
+    auto declarator = g_impl->CreateVariableDeclarator1(
+        context, Es2pandaVariableDeclaratorFlag::VARIABLE_DECLARATOR_FLAG_LET, varIdent,
+        g_impl->VariableDeclaratorInit(context,
+                                       g_impl->VariableDeclarationDeclaratorsConst(context, letStatement, &n)[0]));
 
-    impl->AstNodeIterateConst(context, Ast, FindLet);
-    impl->AstNodeIterateConst(context, Ast, FindMainDef);
+    auto updateDeclaration = g_impl->UpdateVariableDeclaration(
+        context, letStatement, Es2pandaVariableDeclarationKind::VARIABLE_DECLARATION_KIND_LET, &declarator, 1);
+    es2panda_AstNode *newMainStatements[1] = {updateDeclaration};
+    g_impl->BlockStatementSetStatements(context, mainFuncBody, newMainStatements, 1U);
+    g_impl->AstNodeForEach(ast, SetRightParent, context);
+
+    const char *errMessage = g_impl->ContextErrorMessage(context);
+    if (!errMessage) {
+        std::cout << "The errMessage is nullptr." << std::endl;
+        return false;
+    }
+
+    g_impl->AstNodeIterateConst(context, ast, FindLet);
+    size_t declaratorLen = 0;
+    es2panda_AstNode **declarators = g_impl->VariableDeclarationDeclaratorsConst(context, letStatement, &declaratorLen);
+    if (declaratorLen != 1) {
+        std::cout << "Fail to get declartors." << std::endl;
+        return false;
+    }
+
+    char *astStr = g_impl->AstNodeDumpJSONConst(context, ast);
+    expectResStr = expectResStr.erase(0, 1);
+    expectResStr = expectResStr.erase(expectResStr.length() - 1, 1);
+    if (strcmp(expectResStr.c_str(), astStr) != 0) {
+        std::cout << "Dumping ast has wrong result." << std::endl;
+        return false;
+    }
+    es2panda_AstNode *varDeclInit = g_impl->VariableDeclaratorInit(context, declarators[0]);
+    if (!varDeclInit) {
+        std::cout << "The varDeclInit is nullptr." << std::endl;
+        return false;
+    }
+    std::string res1(g_impl->AstNodeDumpEtsSrcConst(context, varDeclInit));
+    if (res1.find("5") == std::string::npos) {
+        std::cout << "Fail to find given variabledeclaration init value." << std::endl;
+        return false;
+    }
+    std::string res2(g_impl->AstNodeDumpEtsSrcConst(context, letStatement));
+    return (res2.find("newVar") != std::string::npos);
+}
+
+/*
+ *   "function main() { \nlet a = 5;\n }"
+ *   is changed to
+ *   "function main() { \nlet newVar = 5;\n }"
+ */
+static bool Handle(es2panda_Context *context, [[maybe_unused]] es2panda_AstNode *ast)
+{
+    g_impl->AstNodeIterateConst(context, ast, FindLet);
+    g_impl->AstNodeIterateConst(context, ast, FindMainDef);
     if (mainScriptFunc == nullptr || letStatement == nullptr) {
         return false;
     }
 
-    auto mainFuncBody = impl->ScriptFunctionBody(context, mainScriptFunc);
-    size_t declaratorLen;
-    es2panda_AstNode **declarators = impl->VariableDeclarationDeclaratorsConst(context, letStatement, &declaratorLen);
-    if (declaratorLen > 0) {
-        for (size_t i = 0; i < declaratorLen; i++) {
-            es2panda_AstNode *declaratorsElement = declarators[i];
-            std::cout << impl->AstNodeDumpJSONConst(context, declaratorsElement) << std::endl;
-            es2panda_AstNode *declaratorInit = impl->VariableDeclaratorInit(context, declaratorsElement);
-
-            std::cout << impl->AstNodeDumpJSONConst(context, declaratorInit) << std::endl;
-
-            es2panda_AstNode *declaratorId = impl->VariableDeclaratorId(context, declaratorsElement);
-            std::cout << impl->AstNodeDumpJSONConst(context, declaratorId) << std::endl;
-
-            // update init
-            std::string className = std::string("newVar");
-            auto *memForName = static_cast<char *>(impl->AllocMemory(context, className.size() + 1, 1));
-            std::copy_n(className.c_str(), className.size() + 1, memForName);
-            size_t n = 0;
-            es2panda_AstNode *varIdent = impl->CreateIdentifier1(context, memForName);
-            auto declarator = impl->CreateVariableDeclarator1(
-                context, Es2pandaVariableDeclaratorFlag::VARIABLE_DECLARATOR_FLAG_LET, varIdent,
-                impl->VariableDeclaratorInit(context,
-                                             impl->VariableDeclarationDeclaratorsConst(context, letStatement, &n)[0]));
-
-            std::cout << impl->AstNodeDumpJSONConst(context, declarator) << std::endl;
-            auto declaration = impl->UpdateVariableDeclaration(
-                context, letStatement, Es2pandaVariableDeclarationKind::VARIABLE_DECLARATION_KIND_LET, &declarator, 1);
-
-            es2panda_AstNode *newMainStatements[1] = {declaration};
-            impl->BlockStatementSetStatements(context, mainFuncBody, newMainStatements, 1U);
-            impl->AstNodeForEach(Ast, SetRightParent, context);
-
-            impl->AstNodeRebind(context, declaration);
-            std::cout << impl->AstNodeDumpJSONConst(context, Ast) << std::endl;
-        }
+    size_t declaratorLen = 0;
+    es2panda_AstNode **declarators = g_impl->VariableDeclarationDeclaratorsConst(context, letStatement, &declaratorLen);
+    if (declaratorLen != 1) {
+        std::cout << "Fail to get declartors." << std::endl;
+        return false;
     }
 
-    return true;
+    es2panda_AstNode *varDeclNode = declarators[0];
+    if (!varDeclNode) {
+        std::cout << "The declaratorsElement is nullptr. " << std::endl;
+        return false;
+    }
+
+    if (!g_impl->IsVariableDeclarator(varDeclNode)) {
+        std::cout << "The varDeclNode is not variable declaration." << std::endl;
+        return false;
+    }
+
+    const es2panda_AstNode *varDeclIDConst = g_impl->VariableDeclaratorIdConst(context, varDeclNode);
+    if (!varDeclIDConst) {
+        std::cout << "The varDeclIDConst is nullptr." << std::endl;
+        return false;
+    }
+    es2panda_AstNode *varDeclID = g_impl->VariableDeclaratorId(context, varDeclNode);
+    if (!varDeclID) {
+        std::cout << "The varDeclID is nullptr." << std::endl;
+        return false;
+    }
+
+    if (strcmp(g_impl->IdentifierName(context, varDeclID), const_cast<char *>("a")) != 0) {
+        std::cout << "The varDeclNode is not the expected node." << std::endl;
+        return false;
+    }
+
+    return UpdateAst(context, ast);
 }
 
 int main(int argc, char **argv)
@@ -175,44 +262,23 @@ int main(int argc, char **argv)
     if (GetImpl() == nullptr) {
         return NULLPTR_IMPL_ERROR_CODE;
     }
-    impl = GetImpl();
 
+    g_impl = GetImpl();
     const char **args = const_cast<const char **>(&(argv[1]));
-    std::cout << args << std::endl;
-    auto config = impl->CreateConfig(argc - 1, args);
-    auto context = impl->CreateContextFromString(config, source.data(), argv[argc - 1]);
-    if (context == nullptr) {
-        std::cerr << "FAILED TO CREATE CONTEXT" << std::endl;
+    auto config = g_impl->CreateConfig(argc - 1, args);
+    g_ctx = g_impl->CreateContextFromString(config, source.data(), argv[argc - 1]);
+    if (g_ctx == nullptr) {
+        std::cerr << "Fail to create context." << std::endl;
         return NULLPTR_CONTEXT_ERROR_CODE;
     }
-
-    impl->ProceedToState(context, ES2PANDA_STATE_PARSED);
-    CheckForErrors("PARSE", context);
-
-    impl->ProceedToState(context, ES2PANDA_STATE_BOUND);
-    CheckForErrors("BOUND", context);
-
-    if (!VariableHandle(context)) {
-        std::cerr << "FAILED TO execute on AfterBind phase" << std::endl;
-        return TEST_ERROR_CODE;
+    int retCode = Test(g_ctx, g_impl, ES2PANDA_STATE_PARSED, Handle);
+    if (retCode != 0) {
+        std::cerr << "Fail to execute on AfterParse phase" << std::endl;
+        g_impl->DestroyConfig(config);
+        return retCode;
     }
 
-    impl->ProceedToState(context, ES2PANDA_STATE_CHECKED);
-    CheckForErrors("CHECKED", context);
-
-    impl->ProceedToState(context, ES2PANDA_STATE_LOWERED);
-    CheckForErrors("LOWERED", context);
-
-    impl->ProceedToState(context, ES2PANDA_STATE_ASM_GENERATED);
-    CheckForErrors("ASM", context);
-
-    impl->ProceedToState(context, ES2PANDA_STATE_BIN_GENERATED);
-    CheckForErrors("BIN", context);
-    if (impl->ContextState(context) == ES2PANDA_STATE_ERROR) {
-        return PROCEED_ERROR_CODE;
-    }
-
-    impl->DestroyConfig(config);
+    g_impl->DestroyConfig(config);
 
     return 0;
 }
