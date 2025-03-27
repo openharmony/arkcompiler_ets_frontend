f8d549def95852c76fbeb26b6f89ce28635a00f9
ZhongNing <zhongning5@huawei.com>
2025-03-27 19:15:31 +0800

    fix issues for arkts-class-static-initialization
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBWQMS
    
    Test scenarios: fix issues
    
    Signed-off-by: ZhongNing <zhongning5@huawei.com>

--- ets2panda/linter/src/lib/TypeScriptLinter.ts
+++ ets2panda/linter/src/lib/TypeScriptLinter.ts
@@ -942,6 +942,7 @@ export class TypeScriptLinter {
     this.checkAssignmentNumericSemanticslyPro(node);
     this.handleInvalidIdentifier(node);
     this.handleExplicitFunctionType(node);
+    this.handleStructPropertyDecl(node);
   }
 
   private handleSendableClassProperty(node: ts.PropertyDeclaration): void {
@@ -1922,7 +1923,6 @@ export class TypeScriptLinter {
     }
 
     this.processClassStaticBlocks(tsClassDecl);
-    this.handleClassStaticPropInit(tsClassDecl);
     this.handleInvalidIdentifier(tsClassDecl);
   }
 
@@ -4308,22 +4308,15 @@ export class TypeScriptLinter {
     }
   }
 
-  private handleClassStaticPropInit(classDecl: ts.ClassDeclaration | ts.ClassExpression): void {
+  private handleStructPropertyDecl(propDecl: ts.PropertyDeclaration): void {
     if (!this.options.arkts2) {
       return;
     }
-    classDecl.members?.forEach((member) => {
-      if (
-        ts.isPropertyDeclaration(member) &&
-        member.modifiers?.some((modifier) => {
-          return modifier.kind === ts.SyntaxKind.StaticKeyword;
-        })
-      ) {
-        if (!member.initializer) {
-          this.incrementCounters(member, FaultID.ClassstaticInitialization);
-        }
-      }
-    });
+    const isStatic = TsUtils.hasModifier(propDecl.modifiers, ts.SyntaxKind.StaticKeyword);
+    const hasNoInitializer = !propDecl.initializer;
+    if (isStatic && hasNoInitializer) {
+      this.incrementCounters(propDecl, FaultID.ClassstaticInitialization);
+    }
   }
 
   private handleTaggedTemplatesExpression(node: ts.Node): void {
