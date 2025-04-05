abefa04975f7748b4f79fe2ceb32c89fbf60632d
钟柠 <zhongning5@huawei.com>
2025-04-05 11:58:21 +0800

    fix issue for swicth expr int
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IBWG59
    
    Test scenarios:new tests update to the linter
    
    Signed-off-by: 钟柠 <zhongning5@huawei.com>

--- ets2panda/linter/src/lib/TypeScriptLinter.ts
+++ ets2panda/linter/src/lib/TypeScriptLinter.ts
@@ -4054,17 +4054,17 @@ export class TypeScriptLinter {
     if (!expressionType) {
       return false;
     }
-    if (TypeScriptLinter.isSwitchAllowedType(expressionType)) {
+    if (this.isSwitchAllowedType(expressionType)) {
       return true;
     }
     if (expressionType.isUnion()) {
       const unionTypes = expressionType.types;
-      return unionTypes.every(TypeScriptLinter.isSwitchAllowedType);
+      return unionTypes.every(this.isSwitchAllowedType);
     }
     return false;
   }
 
-  private static isSwitchAllowedType(type: ts.Type): boolean {
+  private isSwitchAllowedType(type: ts.Type): boolean {
     if (type.flags & (ts.TypeFlags.StringLike | ts.TypeFlags.EnumLike)) {
       return true;
     }
@@ -4074,7 +4074,7 @@ export class TypeScriptLinter {
       return Number.isInteger(value);
     }
 
-    return false;
+    return this.tsTypeChecker.typeToString(type) === 'int';
   }
 
   private handleLimitedLiteralType(literalTypeNode: ts.LiteralTypeNode): void {
