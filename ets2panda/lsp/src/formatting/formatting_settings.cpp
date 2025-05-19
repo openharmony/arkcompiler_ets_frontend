e590619997111a6f49f78e75c60d8ae38d1d7ef6
irfan-karatekin <irfan.karatekin1@huawei-partners.com>
2025-05-20 02:31:46 +0300

    [LSP] FormattingContext
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/IC8637
    Signed-off-by: irfan-karatekin <irfan.karatekin1@huawei-partners.com>

--- ets2panda/lsp/src/formatting/formatting_settings.cpp
+++ ets2panda/lsp/src/formatting/formatting_settings.cpp
@@ -2,9 +2,9 @@
  * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at*
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0*
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,17 @@
  * limitations under the License.
  */
 
-#ifndef BRACE_MATCHING_H
-#define BRACE_MATCHING_H
-
-#include <vector>
-#include "api.h"
-#include "internal_api.h"
+#include "formatting/formatting_settings.h"
 
 namespace ark::es2panda::lsp {
-bool CheckNodeKindForBraceMatching(ark::es2panda::ir::AstNode *node);
-std::vector<TextSpan> GetBraceMatchingAtPosition(es2panda_Context *context, size_t position);
+
+FormatCodeSettings GetDefaultFormatCodeSettings(const std::string &newLineCharacter)
+{
+    FormatCodeSettings settings;
+    if (!(newLineCharacter.empty())) {
+        settings.SetNewLineCharacter(newLineCharacter);
+    }
+    return settings;
+}
 
 }  // namespace ark::es2panda::lsp
-#endif
