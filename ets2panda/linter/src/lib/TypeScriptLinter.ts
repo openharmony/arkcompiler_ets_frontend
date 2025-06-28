c4654ac3e6132b7da81d190b5e06b5124f4cd359
zhongning5 <zhongning5@huawei.com>
2025-06-28 14:29:51 +0800

    fix arkts-no-inferred-generic-params
    Issue:  https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/ICICX6
    
    Test scenarios:Fix bugs for arkts-no-inferred-generic-params
    
    Signed-off-by: zhongning5 <zhongning5@huawei.com>

--- ets2panda/linter/src/lib/TypeScriptLinter.ts
+++ ets2panda/linter/src/lib/TypeScriptLinter.ts
@@ -4804,8 +4804,8 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
   }
 
   private handleGenericCallWithNoTypeArgs(
-    callLikeExpr: ts.CallExpression | ts.NewExpression,
-    callSignature: ts.Signature
+    callLikeExpr: ts.CallExpression | ts.NewExpression | ts.ExpressionWithTypeArguments,
+    callSignature?: ts.Signature
   ): void {
 
     /*
@@ -4819,19 +4819,20 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
       this.incrementCounters(callLikeExpr, FaultID.GenericCallNoTypeArgs, autofix);
       return;
     }
-    this.checkTypeArgumentsForGenericCallWithNoTypeArgs(callLikeExpr, callSignature);
+    if (callSignature) {
+      this.checkTypeArgumentsForGenericCallWithNoTypeArgs(callLikeExpr, callSignature);
+    }
   }
 
-  private static isInvalidBuiltinGenericConstructorCall(newExpression: ts.CallExpression | ts.NewExpression): boolean {
-    if (!ts.isNewExpression(newExpression)) {
-      return false;
-    }
+  private static isInvalidBuiltinGenericConstructorCall(
+    newExpression: ts.CallExpression | ts.NewExpression | ts.ExpressionWithTypeArguments
+  ): boolean {
     const isBuiltin = BUILTIN_GENERIC_CONSTRUCTORS.has(newExpression.expression.getText().replace(/Constructor$/, ''));
     return isBuiltin && (!newExpression.typeArguments || newExpression.typeArguments.length === 0);
   }
 
   private checkTypeArgumentsForGenericCallWithNoTypeArgs(
-    callLikeExpr: ts.CallExpression | ts.NewExpression,
+    callLikeExpr: ts.CallExpression | ts.NewExpression | ts.ExpressionWithTypeArguments,
     callSignature: ts.Signature
   ): void {
     if (ts.isNewExpression(callLikeExpr) && this.isNonGenericClass(callLikeExpr)) {
@@ -4872,7 +4873,7 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
   }
 
   private checkForUnknownTypeInNonArkTS2(
-    callLikeExpr: ts.CallExpression | ts.NewExpression,
+    callLikeExpr: ts.CallExpression | ts.NewExpression | ts.ExpressionWithTypeArguments,
     resolvedTypeArgs: ts.NodeArray<ts.TypeNode>,
     startTypeArg: number
   ): void {
@@ -7141,6 +7142,7 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
     if (node.token === ts.SyntaxKind.ExtendsKeyword || node.token === ts.SyntaxKind.ImplementsKeyword) {
       node.types.forEach((type) => {
         const expr = type.expression;
+        this.handleGenericCallWithNoTypeArgs(type);
         if (ts.isCallExpression(expr)) {
           this.incrementCounters(expr, FaultID.ExtendsExpression);
           return;
