2b67ee700d9657ed55a470683c803222b930eb46
oh-rgx <renguangxuan@huawei.com>
2025-07-19 15:01:56 +0800

    Fix check spreadElement crash
    Issue: #ICNE4C
    
    Signed-off-by: oh-rgx <renguangxuan@huawei.com>

--- ets2panda/checker/ETSAnalyzer.cpp
+++ ets2panda/checker/ETSAnalyzer.cpp
@@ -380,7 +380,7 @@ checker::Type *ETSAnalyzer::Check(ir::SpreadElement *expr) const
     if (expr->PreferredType() != nullptr) {
         expr->Argument()->SetPreferredType(expr->PreferredType());
     }
-    auto type = expr->AsSpreadElement()->Argument()->Check(checker);
+    auto type = expr->Argument()->Check(checker);
     Type *exprType = type->MaybeBaseTypeOfGradualType();
 
     if (exprType->IsETSResizableArrayType()) {
