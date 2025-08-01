214ce75fce5b134b1b3084c49264cd6a13cb71b5
zengzengran <zengran2@huawei.com>
2025-08-01 15:00:07 +0800

    Fix namespace same name builtin class crash
    Issue:
    https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/ICQDFV
    
    Description:
    For namespace, it is permissible to define classes and interfaces with the same names as built-in ones, and it should not return the built-in ETSObjectType.
    
    Tested-by:
    ninja tests (passed)
    ets_testrunner (passed)
    
    Signed-off-by: zengzengran <zengran2@huawei.com>#

--- ets2panda/checker/ets/typeCreation.cpp
+++ ets2panda/checker/ets/typeCreation.cpp
@@ -350,13 +350,22 @@ static ETSObjectType *InitializeGlobalBuiltinObjectType(ETSChecker *checker, Glo
     }
 }
 
+static bool DeclarationInNameSpace(ETSChecker *checker)
+{
+    auto *conclass = checker->Context().ContainingClass();
+    if (conclass == nullptr || conclass->GetDeclNode() == nullptr || !conclass->GetDeclNode()->IsClassDefinition()) {
+        return false;
+    }
+    return conclass->GetDeclNode()->AsClassDefinition()->IsNamespaceTransformed();
+}
+
 ETSObjectType *ETSChecker::CreateETSObjectTypeOrBuiltin(ir::AstNode *declNode, ETSObjectFlags flags)
 {
     if (LIKELY(HasStatus(CheckerStatus::BUILTINS_INITIALIZED))) {
         return CreateETSObjectType(declNode, flags);
     }
     auto const globalId = GetGlobalTypesHolder()->NameToId(GetObjectTypeDeclNames(declNode).first);
-    if (!globalId.has_value()) {
+    if (!globalId.has_value() || DeclarationInNameSpace(this)) {
         return CreateETSObjectType(declNode, flags);
     }
     return InitializeGlobalBuiltinObjectType(this, globalId.value(), declNode, flags);
