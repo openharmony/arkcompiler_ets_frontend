3277c968fe5c29fda8d496875bbb50b1408aecdb
wangjiahui <wangjiahui27@h-partners.com>
2025-08-08 14:43:44 +0800

    0702 sync 0728
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/ICS1NG?from=project-issue
    Signed-off-by: wangjiahui <wangjiahui27@h-partners.com>

--- ets2panda/linter/src/lib/utils/TsUtils.ts
+++ ets2panda/linter/src/lib/utils/TsUtils.ts
@@ -47,13 +47,16 @@ import { STRINGLITERAL_NUMBER, STRINGLITERAL_NUMBER_ARRAY } from './consts/Strin
 import { ETS_MODULE, PATH_SEPARATOR, VALID_OHM_COMPONENTS_MODULE_PATH } from './consts/OhmUrl';
 import { EXTNAME_ETS, EXTNAME_JS, EXTNAME_D_ETS } from './consts/ExtensionName';
 import { CONCAT_ARRAY, STRING_ERROR_LITERAL } from './consts/Literals';
+import { INT_MIN, INT_MAX } from './consts/NumericalConstants';
 
 export const PROMISE_METHODS = new Set(['all', 'race', 'any', 'resolve', 'allSettled']);
+export const PROMISE_METHODS_WITH_NO_TUPLE_SUPPORT = new Set(['all', 'race', 'any', 'allSettled']);
 export const SYMBOL = 'Symbol';
 export const SYMBOL_CONSTRUCTOR = 'SymbolConstructor';
 const ITERATOR = 'iterator';
 
 export type CheckType = (this: TsUtils, t: ts.Type) => boolean;
+
 export class TsUtils {
   constructor(
     private readonly tsTypeChecker: ts.TypeChecker,
@@ -801,6 +804,9 @@ export class TsUtils {
     if (this.skipStructuralTypingCheckForFunctionals(lhsType, rhsType)) {
       return false;
     }
+    if (this.skipCheckForArrayBufferLike(lhsType, rhsType)) {
+      return false;
+    }
     if (rhsType.isUnion() || lhsType.isUnion()) {
       return this.needToDeduceStructuralIdentityHandleUnions(lhsType, rhsType, rhsExpr, isStrict);
     }
@@ -895,6 +901,16 @@ export class TsUtils {
       isStrictLhs = this.isSendableClassOrInterface(lhsType);
     }
     return isStrictLhs && this.typeContainsNonSendableClassOrInterface(rhsType);
+  }
+
+  skipCheckForArrayBufferLike(lhsType: string | ts.Type, rhsType: string | ts.Type): boolean {
+    const lhsIsArrayBufferLike = TsUtils.isArrayBufferType(
+      typeof lhsType === 'string' ? lhsType : this.tsTypeChecker.typeToString(lhsType)
+    );
+    const rhsIsArrayBufferLike = TsUtils.isArrayBufferType(
+      typeof rhsType === 'string' ? rhsType : this.tsTypeChecker.typeToString(rhsType)
+    );
+    return !!this.options.arkts2 && lhsIsArrayBufferLike && rhsIsArrayBufferLike;
   }
 
   private processExtendedParentTypes(typeA: ts.Type, typeB: ts.Type): boolean {
@@ -1669,6 +1685,21 @@ export class TsUtils {
     return false;
   }
 
+  isStdLongType(type: ts.Type | undefined): boolean {
+    if (!type) {
+      return false;
+    }
+    const sym = type.aliasSymbol;
+    return !!sym && sym.getName() === 'long' && this.isGlobalSymbol(sym);
+  }
+
+  static ifLargerThanInt(node: ts.NumericLiteral, isPrefix: boolean): boolean {
+    const raw = node.getText();
+    const value = isPrefix ? Number(raw) * -1 : Number(raw);
+
+    return value < INT_MIN || value > INT_MAX;
+  }
+
   isStdErrorType(type: ts.Type): boolean {
     const symbol = type.symbol;
     if (!symbol) {
@@ -2094,6 +2125,11 @@ export class TsUtils {
   static isFunctionalType(type: ts.Type): boolean {
     const callSigns = type.getCallSignatures();
     return callSigns && callSigns.length > 0;
+  }
+
+  static isArrayBufferType(typeStr: string): boolean {
+    const arrayBufferLikeArr = ['ArrayBuffer', 'ArrayBufferLike'];
+    return arrayBufferLikeArr.includes(typeStr);
   }
 
   static getFunctionalTypeSignature(type: ts.Type): ts.Signature | undefined {
@@ -3861,16 +3897,24 @@ export class TsUtils {
     }
   }
 
- isNumberArrayType(type: ts.Type): boolean {
-      if (!type.symbol || !this.isGenericArrayType(type)) {
-        return false;
-      }
+  static isNegativeNumericLiteral(expr: ts.Expression): boolean {
+    return (
+      ts.isPrefixUnaryExpression(expr) &&
+      expr.operator === ts.SyntaxKind.MinusToken &&
+      ts.isNumericLiteral(expr.operand)
+    );
+  }
 
-      const typeArguments = this.tsTypeChecker.getTypeArguments(type as ts.TypeReference);
-      if (!typeArguments || typeArguments.length === 0) {
-        return false;
-      }
-
-      return (typeArguments[0].flags & ts.TypeFlags.Number) !== 0;
+  isNumberArrayType(type: ts.Type): boolean {
+    if (!type.symbol || !this.isGenericArrayType(type)) {
+      return false;
     }
+
+    const typeArguments = this.tsTypeChecker.getTypeArguments(type);
+    if (!typeArguments || typeArguments.length === 0) {
+      return false;
+    }
+
+    return (typeArguments[0].flags & ts.TypeFlags.Number) !== 0;
+  }
 }
