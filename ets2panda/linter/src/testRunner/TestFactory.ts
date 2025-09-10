2c1e291df85f9eb82cef319fd05d7e2c313dbc10
youdongzhen <youdongzhen@h-partners.com>
2025-09-10 17:28:18 +0800

    fix test
    Issue: https://gitee.com/openharmony/arkcompiler_ets_frontend/issues/ICXDMP
    
    Signed-off-by: youdongzhen <youdongzhen@h-partners.com>
    Change-Id: I6cab46c8aff6c45e7c75d1450f182c30fc2d5020

--- ets2panda/linter/src/testRunner/TestFactory.ts
+++ ets2panda/linter/src/testRunner/TestFactory.ts
@@ -125,8 +125,9 @@ function getLinterOptionsFromCommandLine(cmdLine: string): LinterOptions {
 function getDefaultTestOptions(): LinterOptions {
   return {
     useRtLogic: true,
-    checkTsAsSource:
-      true /* By default, treat any test file with '.ts' extension as a source file (as opposed to library) */,
+
+    /* By default, treat any test file with '.ts' extension as a source file (as opposed to library) */
+    checkTsAsSource: true,
     compatibleSdkVersion: 12,
     compatibleSdkVersionStage: 'beta3',
     checkTsAndJs: true
