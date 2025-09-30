b1783734adf862f4e07f68bfffa5965a40c782fb
da_wei_li11 <lidawei30@huawei.com>
2025-09-30 16:39:34 +0800

    libpandabase header file path modification
    Issue:https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/7700
    
    Signed-off-by: da_wei_li11 <lidawei30@huawei.com>

--- merge_abc/src/assemblyTypeProto.h
+++ merge_abc/src/assemblyTypeProto.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +16,7 @@
 #ifndef MERGE_ABC_ASSEMBLY_TYPE_H
 #define MERGE_ABC_ASSEMBLY_TYPE_H
 
-#include "arena_allocator.h"
+#include "mem/arena_allocator.h"
 #include "assemblyType.pb.h"
 #include "assembly-program.h"
 
