acde08636511aa162e057ce3c432c467414a775e
Alexander Andreev <Alexander.Andreev@mir.dev>
2025-11-21 09:10:46 +0300

    [AST] Reorder and lowering String constants
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/8395
    Signed-off-by: alexanderandreevmir <alexanderandreevmir@gmail.com>

--- ets2panda/compiler/lowering/ets/stringConstantsLowering.cpp
+++ ets2panda/compiler/lowering/ets/stringConstantsLowering.cpp
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,35 +18,111 @@
 
 namespace ark::es2panda::compiler {
 
+enum class ChildLocation { LEFT_CHILD, RIGHT_CHILD };
+
 std::string_view StringConstantsLowering::Name() const
 {
     return "StringConstantsLowering";
 }
 
-static ir::AstNode *FoldConcat(public_lib::Context *ctx, ir::BinaryExpression *const concat)
+static ChildLocation IsLeftOrRight(ir::Expression *const child)
 {
-    auto const lhs = concat->Left()->AsStringLiteral();
-    auto const rhs = concat->Right()->AsStringLiteral();
-    auto const resStr = util::UString(lhs->Str().Mutf8() + rhs->Str().Mutf8(), ctx->allocator).View();
+    ES2PANDA_ASSERT(child->Parent()->IsBinaryExpression());
+    auto const binOpParent = child->Parent()->AsBinaryExpression();
+    auto const leftExprParent = binOpParent->Left();
+    return (leftExprParent == child) ? ChildLocation::LEFT_CHILD : ChildLocation::RIGHT_CHILD;
+}
+
+static bool IsBinaryExpressionPlus(ir::AstNode *const node)
+{
+    if (node->IsBinaryExpression()) {
+        auto const binOp = node->AsBinaryExpression();
+        return binOp->OperatorType() == lexer::TokenType::PUNCTUATOR_PLUS;
+    }
+    return false;
+}
+
+/*
+<---AST before (input to optimization)--->
+        BinaryExpression
+                +
+        /              \
+ StringLiteral   StringLiteral
+
+<---AST after (output from optimization)--->
+ StringLiteral (combined from two)
+*/
+static ir::AstNode *FoldStringLiterals(public_lib::Context *ctx, ir::StringLiteral *const leftStrLit,
+                                       ir::StringLiteral *const rightStrLit)
+{
+    auto const resStr = util::UString(leftStrLit->Str().Mutf8() + rightStrLit->Str().Mutf8(), ctx->allocator).View();
 
     auto resNode = util::NodeAllocator::Alloc<ir::StringLiteral>(ctx->allocator, resStr);
     ES2PANDA_ASSERT(resNode != nullptr);
-    resNode->SetParent(concat->Parent());
-    resNode->SetRange({lhs->Range().start, rhs->Range().end});
+    resNode->SetRange({leftStrLit->Range().start, rightStrLit->Range().end});
     return resNode;
+}
+
+/*
+<---AST before (input to optimization)--->
+             BinaryExpression
+                     +
+            /                \
+    BinaryExpression   StringLiteral
+            +
+    /              \
+Identifier   StringLiteral
+
+<---AST after (output from optimization)--->
+     BinaryExpression
+             +
+    /                \
+Identifier    BinaryExpression
+                      +
+              /              \
+       StringLiteral   StringLiteral
+*/
+static ir::AstNode *ReorderConcat(public_lib::Context *ctx, ir::StringLiteral *leftStrLit,
+                                  ir::StringLiteral *rightStrLit)
+{
+    auto const parentLeftSL = leftStrLit->Parent()->AsBinaryExpression();
+    auto const parentRightSL = rightStrLit->Parent()->AsBinaryExpression();
+    if (parentLeftSL->Parent()->IsBinaryExpression()) {
+        auto const parentParentLeftSL = parentLeftSL->Parent()->AsBinaryExpression();
+        auto const leftExprParentSL = parentLeftSL->Left();
+
+        const ChildLocation childLoc1 = IsLeftOrRight(parentLeftSL);
+        const ChildLocation childLoc2 = IsLeftOrRight(rightStrLit);
+
+        (childLoc1 == ChildLocation::LEFT_CHILD) ? parentParentLeftSL->SetLeft(leftExprParentSL)
+                                                 : parentParentLeftSL->SetRight(leftExprParentSL);
+        auto foldedNode = FoldStringLiterals(ctx, leftStrLit, rightStrLit);
+        (childLoc2 == ChildLocation::LEFT_CHILD) ? parentRightSL->SetLeft(foldedNode->AsStringLiteral())
+                                                 : parentRightSL->SetRight(foldedNode->AsStringLiteral());
+        return foldedNode;
+    }
+    return rightStrLit;
 }
 
 bool StringConstantsLowering::PerformForModule(public_lib::Context *ctx, parser::Program *program)
 {
+    ir::StringLiteral *firstFoundSL = nullptr;
     program->Ast()->TransformChildrenRecursivelyPostorder(
-        [ctx](checker::AstNodePtr const node) -> checker::AstNodePtr {
-            if (node->IsBinaryExpression()) {
-                auto const binOp = node->AsBinaryExpression();
-                if (binOp->OperatorType() == lexer::TokenType::PUNCTUATOR_PLUS && binOp->Left()->IsStringLiteral() &&
-                    binOp->Right()->IsStringLiteral()) {
-                    return FoldConcat(ctx, binOp);
+        [&](checker::AstNodePtr const node) -> checker::AstNodePtr {
+            if (node->IsStringLiteral() && IsBinaryExpressionPlus(node->AsStringLiteral()->Parent())) {
+                auto currNodeSL = node->AsStringLiteral();
+                if (firstFoundSL != nullptr && firstFoundSL->Parent() != currNodeSL->Parent()) {
+                    auto resNode = ReorderConcat(ctx, firstFoundSL, currNodeSL);
+                    firstFoundSL = resNode->AsStringLiteral();
+                    return resNode;
+                } else {
+                    firstFoundSL = currNodeSL;
+                    return node;
                 }
             }
+            if (firstFoundSL != nullptr && !IsBinaryExpressionPlus(node))
+                firstFoundSL = nullptr;
+
             return node;
         },
         Name());
