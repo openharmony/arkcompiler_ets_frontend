38b382abf1d4f8918559097a9ff2d747e56a2ee5
wangzhengji <wangzhengji3@huawei.com>
2026-01-09 10:40:21 +0800

    Fix unexpected smartCast error
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8808
    
    Signed-off-by: wangzhengji <wangzhengji3@huawei.com>

--- ets2panda/checker/checkerContext.cpp
+++ ets2panda/checker/checkerContext.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -199,6 +199,7 @@ void CheckerContext::ExitLoop(SmartCastArray &prevSmartCasts, bool const clearFl
     if (clearFlag) {
         status_ &= ~CheckerStatus::IN_LOOP;
     }
+    status_ &= ~(CheckerStatus::MEET_BREAK | CheckerStatus::MEET_CONTINUE);
 
     if (!breakSmartCasts_.empty()) {
         auto it = breakSmartCasts_.begin();
