15143c5064721c340cb75dfa7851d56d114f19d1
Dmitry Pimenov <pimenov.dmitry@huawei-partners.com>
2026-01-13 17:00:23 +0300

    Inferred type instead of this type for receiver
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8909
    Signed-off-by: Dmitry Pimenov <pimenov.dmitry@huawei-partners.com>

--- ets2panda/checker/ETSAnalyzerHelpers.cpp
+++ ets2panda/checker/ETSAnalyzerHelpers.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -665,7 +665,7 @@ checker::Type *InferReturnType(ETSChecker *checker, ir::ScriptFunction *containi
     }
     containingFunc->Signature()->RemoveSignatureFlag(checker::SignatureFlags::NEED_RETURN_TYPE);
     if (stArgument != nullptr && stArgument->IsIdentifier() && stArgument->OriginalNode() != nullptr &&
-        stArgument->OriginalNode()->IsThisExpression()) {
+        stArgument->OriginalNode()->IsThisExpression() && containingFunc->IsMethod()) {
         containingFunc->Signature()->AddSignatureFlag(SignatureFlags::THIS_RETURN_TYPE);
     } else {
         containingFunc->Signature()->AddSignatureFlag(checker::SignatureFlags::INFERRED_RETURN_TYPE);
