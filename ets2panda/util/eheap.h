10d18330a4697ab1ab3ce98557c3a966e54434b9
Tatiana Titova <titova.tatiana@huawei-partners.com>
2026-01-14 12:47:17 +0300

    Fix size of a raw pointers
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8914
    Signed-off-by: Tatiana Titova <titova.tatiana@huawei-partners.com>

--- ets2panda/util/eheap.h
+++ ets2panda/util/eheap.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -128,8 +128,8 @@ public:
             if (raw == 0) {
                 return nullptr;
             }
-            auto ptr =
-                reinterpret_cast<T *>(ToUintPtr(gEHeapSpace->BaseAddr()) + (raw << EHeapSpace::ALLOC_LOG_ALIGNMENT));
+            auto ptr = reinterpret_cast<T *>(ToUintPtr(gEHeapSpace->BaseAddr()) +
+                                             (static_cast<uintptr_t>(raw) << EHeapSpace::ALLOC_LOG_ALIGNMENT));
             gEHeapSpace->AssertInRange(ptr);
             return ptr;
         }
