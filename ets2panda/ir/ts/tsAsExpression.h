160ff4dd3e59479db7ac8dd0b71d0ab7a718e0a4
zhaoshuting <zhaoshuting2@huawei.com>
2026-01-21 14:42:50 +0800

    Fix missing cte for as expression in rollback
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8964
    
    Signed-off-by: zhaoshuting <zhaoshuting2@huawei.com>

--- ets2panda/ir/ts/tsAsExpression.h
+++ ets2panda/ir/ts/tsAsExpression.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -71,6 +71,7 @@ public:
     void Compile([[maybe_unused]] compiler::ETSGen *etsg) const override;
     checker::Type *Check([[maybe_unused]] checker::TSChecker *checker) override;
     checker::VerifiedType Check([[maybe_unused]] checker::ETSChecker *checker) override;
+    void CleanCheckInformation() override;
 
     void Accept(ASTVisitorT *v) override
     {
