70ec6d70f1a78a8d901971043c53f999150119f5
HuSenlin <husenlin@huawei.com>
2026-01-23 16:17:00 +0800

    Collections:Supplement-Special-Scenarios
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/8984
    
    Signed-off-by: HuSenlin <husenlin@huawei.com>

--- ets2panda/linter/src/lib/TypeScriptLinter.ts
+++ ets2panda/linter/src/lib/TypeScriptLinter.ts
@@ -9845,7 +9845,7 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
 
   private processPropertyAccessExprForCollections(node: ts.Node, ident: ts.Node): void {
     let isSpecialScene: boolean = false;
-    if (TypeScriptLinter.isSpecialContainerScene(node, ident)) {
+    if (this.isSpecialContainerScene(node, ident)) {
       isSpecialScene = true;
       this.updateImportAutofix(node, '');
     }
@@ -9904,7 +9904,7 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
     });
   }
 
-  private static isSpecialContainerScene(node: ts.Node, ident: ts.Node): boolean {
+  private isSpecialContainerScene(node: ts.Node, ident: ts.Node): boolean {
     const parent = node.parent;
     if (!ts.isNewExpression(parent)) {
       return false;
@@ -9920,11 +9920,18 @@ export class TypeScriptLinter extends BaseTypeScriptLinter {
     const args = parent.arguments;
     if (args && args.length === 1) {
       const arg = args[0];
-      if (ts.isNumericLiteral(arg)) {
-        return true;
-      }
+      return this.isNumberTypeExpression(arg);
     }
     return false;
+  }
+
+  private isNumberTypeExpression(arg: ts.Expression): boolean {
+    if (ts.isNumericLiteral(arg)) {
+      return true;
+    }
+
+    const argType = this.tsTypeChecker.getTypeAtLocation(arg);
+    return !!(argType?.flags & ts.TypeFlags.Number);
   }
 
   private checkCorrespondingQualifiedName(node: ts.Node, isSpecialScene: boolean): void {
