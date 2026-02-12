e4a762e07860e21fcaf7fc021bcc29506aacea92
tension16 <tension16@noreply.gitcode.com>
2026-02-12 10:44:05 +0800

    Revert "fix field override"
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/9111
    Signed-off-by: zhangli <1113989231@qq.com>

--- ets2panda/compiler/core/ETSfunction.h
+++ ets2panda/compiler/core/ETSfunction.h
@@ -38,11 +38,9 @@ private:
     static void CompileSourceBlock(ETSGen *etsg, const ir::BlockStatement *block);
     static void CompileAsStaticBlock(ETSGen *etsg);
     static void CompileAsConstructor(ETSGen *etsg, const ir::ScriptFunction *scriptFunc);
-    static void CompileInstanceFieldInitializers(ETSGen *etsg);
     static void CompileFunction(ETSGen *etsg);
     static void CallImplicitCtor(ETSGen *etsg);
     static void ExtendWithDefaultReturn(ETSGen *etsg, const ir::AstNode *node, const ir::ScriptFunction *scriptFunc);
-    static void CompileConstructorWithExplicitSuper(ETSGen *etsg, const ArenaVector<ir::Statement *> &statements);
 };
 }  // namespace ark::es2panda::compiler
 
