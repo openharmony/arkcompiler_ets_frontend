69fcd167b968a52c6dc39e1b26e983630c828455
ozerovnikita <ozerov.nikita@huawei-partners.com>
2026-04-15 18:44:34 +0300

    improve diagnostics and warn lists struct check
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/9813
    
    Co-Authored-By: Agent
    Signed-off-by: ozerovnikita <ozerov.nikita@huawei-partners.com>

--- ets2panda/util/diagnostic/diagnostic.rb
+++ ets2panda/util/diagnostic/diagnostic.rb
@@ -47,7 +47,7 @@ module Diagnostic
       configuration_error("Lists are only allowed for 'warning'. Found in '#{diagnostic_type}' for '#{diagnostic.name}'")
     end
 
-    paths =[]
+    paths = []
     list.each do |bl_name|
       if result_map.key?(bl_name)
         paths.concat(result_map[bl_name])
@@ -56,7 +56,7 @@ module Diagnostic
       end
     end
     paths.uniq!
-    paths.map! { |path| path.gsub('/', '[\\\\\\\\\\\\\\\\/]') } # hack for win path
+    paths.map! { |path| path.gsub('/', '[\\\\\\\\\\\\\\\\/]') } # HACK: for win path
     paths
   end
 
@@ -81,59 +81,116 @@ module Diagnostic
       end
       return true
     end
-    return false
+    false
   end
 
   def validate_path_pattern!(path, context)
     allowed = 'a-zA-Z0-9_\\-./\\\\*+?\\[\\]()@'
-    regex = /\A\.\*\/[#{allowed}]+(?:\.[*]|(?:\\.[a-zA-Z]+)+)\z/
+    regex = %r{\A\.\*/[#{allowed}]+(?:\.[*]|(?:\\.[a-zA-Z]+)+)\z}
 
-    unless path.match?(regex)
-      if !path.start_with?('.*/')
-        regex_error("Path '#{path}' in #{context} must start with '.*/'")
-      elsif !path.end_with?('.*') && !path.match?(/(\\.[a-zA-Z]+)+$/)
-        regex_error("Path '#{path}' in #{context} must end with '.*' or '\\.[a-z]+(\\.[a-z]+)*'")
-      else
-        regex_error("Path '#{path}' in #{context} contains forbidden characters.
+    return if path.match?(regex)
+
+    if !path.start_with?('.*/')
+      regex_error("Path '#{path}' in #{context} must start with '.*/'")
+    elsif !path.end_with?('.*') && !path.match?(/(\\.[a-zA-Z]+)+$/)
+      regex_error("Path '#{path}' in #{context} must end with '.*' or '\\.[a-z]+(\\.[a-z]+)*'")
+    else
+      regex_error("Path '#{path}' in #{context} contains forbidden characters.
                      Allowed: a-z, A-Z, 0-9, _, -, ., /, \\, *, +, ?, [, ], (, ), @")
-      end
     end
   end
 
+  def check_if_warnings_list(data)
+    data.respond_to?(:hardlist) || data.respond_to?(:softlist)
+  end
+
   def verify_lists(data)
-    return unless data.respond_to?(:hardlist) || data.respond_to?(:softlist)
+    return unless check_if_warnings_list(data)
+
+    if data.respond_to?(:hardlist) && data.respond_to?(:softlist)
+      configuration_error('Both hardlist and softlist defined in the same file, only one is allowed')
+    end
 
     list = data.respond_to?(:hardlist) ? data.hardlist : data.softlist
     list_name = data.respond_to?(:hardlist) ? 'hardlist' : 'softlist'
 
+    allowed_item_fields = Set['name', 'paths']
     names = Set.new
     list.each do |item|
+      item.each_pair do |field, _value|
+        unless allowed_item_fields.include?(field.to_s)
+          configuration_error("Unknown field '#{field}' in #{list_name} item '#{item.name}'")
+        end
+      end
+
+      configuration_error("Missing 'name' field in #{list_name} item") unless item.respond_to?(:name) && item.name
+      unless item.respond_to?(:paths) && item.paths
+        configuration_error("Missing 'paths' field in #{list_name} item '#{item.name}'")
+      end
+
       configuration_error("Duplicate name '#{item.name}' in #{list_name}") if names.include?(item.name)
       names.add(item.name)
 
-      next unless item.paths
       item.paths.each do |path|
         validate_path_pattern!(path, "#{list_name} '#{item.name}'")
       end
     end
   end
 
-  def set_lists(diagnostic, diagnostic_type)
-    if diagnostic.respond_to?(:hardlist) && diagnostic.hardlist
-      diagnostic.hardlist = create_diagnostic_pathlist(diagnostic_type, diagnostic, diagnostic.hardlist, @hardlists_map)
-    else
-      diagnostic.hardlist = []
+  def verify_diagnostics_yamls(data)
+    return if check_if_warnings_list(data)
+
+    valid_kinds = Set[
+      'warning', 'syntax', 'semantic', 'fatal',
+      'declgen_ets2ts_error', 'declgen_ets2ts_warning',
+      'isolated_declgen', 'arkts_config_error'
+    ]
+    allowed_fields = Set['name', 'id', 'message', 'strict', 'softlist', 'hardlist', 'code_fix_ids']
+
+    diagnostic_types = data.each_pair.map { |k, _| k.to_s }.select { |k| valid_kinds.include?(k) }
+
+    if diagnostic_types.size > 1
+      configuration_error("Multiple diagnostic kinds in one file: #{diagnostic_types.join(', ')}")
     end
 
-    if diagnostic.respond_to?(:softlist) && diagnostic.softlist
-      diagnostic.softlist = create_diagnostic_pathlist(diagnostic_type, diagnostic, diagnostic.softlist, @softlists_map)
-    else
-      diagnostic.softlist = []
+    data.each_pair do |key, _|
+      key_str = key.to_s
+      unless valid_kinds.include?(key_str) || key_str == 'graveyard'
+        configuration_error("Unknown diagnostic kind '#{key_str}'")
+      end
     end
+
+    data.each_pair do |diagnostic_type, diagnostics|
+      next unless valid_kinds.include?(diagnostic_type.to_s)
+
+      diagnostics.each do |diagnostic|
+        diagnostic.each_pair do |field, _|
+          unless allowed_fields.include?(field.to_s)
+            configuration_error("Unknown field '#{field}' in #{diagnostic_type} diagnostic '#{diagnostic.name}'")
+          end
+        end
+      end
+    end
+  end
+
+  def set_lists(diagnostic, diagnostic_type)
+    diagnostic.hardlist = if diagnostic.respond_to?(:hardlist) && diagnostic.hardlist
+                            create_diagnostic_pathlist(diagnostic_type, diagnostic, diagnostic.hardlist, @hardlists_map)
+                          else
+                            []
+                          end
+
+    diagnostic.softlist = if diagnostic.respond_to?(:softlist) && diagnostic.softlist
+                            create_diagnostic_pathlist(diagnostic_type, diagnostic, diagnostic.softlist, @softlists_map)
+                          else
+                            []
+                          end
   end
 
   def wrap_data(data)
     return if try_parse_lists(data)
+
+    verify_diagnostics_yamls(data)
 
     graveyard = data.delete_field(:graveyard)
     data.freeze
@@ -158,7 +215,7 @@ module Diagnostic
           normalization_error
         end
       end
-      diagnostics.map(&:id).group_by(&:itself).select{ |_, v| v.size > 1 }.map(&:first).each do |duplicate|
+      diagnostics.map(&:id).group_by(&:itself).select { |_, v| v.size > 1 }.map(&:first).each do |duplicate|
         warn "Duplicate id '#{duplicate}' for diagnostic type '#{diagnostic_type}'"
         normalization_error
       end
